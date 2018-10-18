f1c22d88484403115e34c83d9fb86e5d403edec1
cslee7389 <cslee@squincy.com>
2018-10-18 17:18:16 +0800

    update svn

--- resources/views/staradmin/client/details.blade.php
+++ resources/views/staradmin/client/details.blade.php
@@ -1,6 +1,6 @@
-@extends('layouts.layout')
+@extends('layouts.default')
 
-@section('title', '阿福街')
+@section('title', '商品详情')
 
 @section('top-css')
     @parent
@@ -10,29 +10,26 @@
 @section('content')	
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-@if(!empty($record)) 
 
-		<div class="card" style="background-color: #f5f3f3;">
-			<a href="{{ redirect()->back()->getTargetUrl() }}" class="btn btn-default" >@lang('dingsu.back')</a>
-        </div>
+@if(!empty($record)) 
 
         <div class="wrapper img-product">
 			<img src="{{$record->product_picurl}}" alt="product details" class="product_details" />
         </div>
 		<div class="wrapper full-height">
 			<div class="card">
-				<div class="col-xs-6 promo-price">
-					<span class="number">{{$record->product_price}}</span> 试用价
-				</div>
-				<div class="col-xs-6 price">
-					<div class="normal-title">淘宝正常售价：</div>
-					<div class="normal-number">{{$record->product_price}} 元</div>
-				</div>
-			</div>
-			
-			<div class="card">
 				<div class="col-xs-12 description">
 					{{$record->product_name}}
+				</div>
+				<div class="clear"></div>
+			</div>
+
+			<div class="card">
+				<div class="col-xs-5 promo-price">
+					<span class="number">¥ {{$record->product_price}}</span>
+				</div>
+				<div class="col-xs-7">
+					<span class="strikethrough-text">淘宝正常售价:¥ {{$record->product_price}}</span>
 				</div>
 				<div class="clear"></div>
 			</div>
@@ -40,22 +37,12 @@
 			<div class="card">
 				<!-- We can get rid of the strikethrough look by adding a span tag and applying a background color to the span. Applying a background to the h2:after won't work because it won't adjust and hug the text. -->
 				<div class="voucher-header">
-					<span>独家优惠卷</span>
+					<span>复制优惠卷代码 打开手机淘宝即可优惠</span>
 					<div class="voucher">
-						<div class="code">¥ {{$record->voucher_id}} ¥</div>
-						<div>请复制后，打开淘宝APP就能看到商品。</div>
+						<div class="code"> {{$record->voucher_id}}
+						</div>
 					</div>
 				</div>
-			</div>
-			
-			<div class="card">
-				<div class="col-xs-12 instruction">
-					使用方法：<br />
-					第一步： 复制上方橙色文字的淘口令。<br />
-					第二步： 打开淘宝APP。<br />
-					第三步： 领取优惠卷，下单购买。<br />					
-				</div>
-				<div class="clear"></div>
 			</div>
 			
 			<div class="row">
