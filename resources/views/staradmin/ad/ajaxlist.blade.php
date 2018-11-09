693d6af523c2658bc5562054222416701957fd3c
cslee7389 <cslee@squincy.com>
2018-11-10 00:35:55 +0800

    update css

--- resources/views/staradmin/ad/ajaxlist.blade.php
+++ resources/views/staradmin/ad/ajaxlist.blade.php
@@ -1,14 +1,4 @@
-<style>
 
-	@media only screen and (max-width: 320px) {
-
-	   body { 
-	      font-size: 2em; 
-	   }
-
-	}
-	
-	</style>
 @foreach($result as $item)
 	<li class="dbox">
 		<a href="#" class="imgBox dbox0">
@@ -17,14 +7,14 @@
 		<div class="dbox1">
 			<h2>{{$item->product_description}}</h2>
 			<h3>
-				<font style="color: #fa8100; font-size:4vw;">券后价¥{{$item->discount_price}}</font>
+				<font>券后价¥{{$item->discount_price}}</font>
 				淘宝原价{{$item->product_price}}元
 			</h3>
 			<div class="quan">
 
-				<span style="font-size:4vw;">¥{{$item->required_point}}元券
+				<span>¥{{$item->required_point}}元券
 				@if ($item->product_quantity > 0)
-					<font style="color:#81be07;">剩 {{$item->product_quantity}} 张</font>
+					<font>剩 {{$item->product_quantity}} 张</font>
 				@endif
 				</span>
 				<a data-id="{{$item->redeem_code}}" id="{{$item->redeem_code}}">微信领取</a>
