256e5f9dedddbf3da0204c6a1e3eb38e6a07fd63
cslee7389 <cslee@squincy.com>
2018-12-05 12:48:05 +0800

    update js css

--- resources/views/staradmin/layouts/app.blade.php
+++ resources/views/staradmin/layouts/app.blade.php
@@ -9,8 +9,8 @@
         <title>{{env('APP_NAME')}} - @yield('title')</title>
 		
 		@section('top-css')
-			<link href="//netdna.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
-			<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+			<link href="{{ asset('/client/css/bootstrap.min.css') }}" rel="stylesheet">
+			<link rel="stylesheet" href="{{ asset('/client/css/all.css') }}" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
 			<link rel="stylesheet" href="{{ asset('/test/main/css/public.css') }}" />
 			<link rel="stylesheet" href="{{ asset('/test/main/css/module.css') }}" />
 			<link rel="stylesheet" href="{{ asset('/test/main/css/style.css') }}" />
@@ -55,10 +55,10 @@
 		@show
 		
 		@section('top-javascript')
-			<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.6.1/angular.js"></script>
-			<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.6.1/angular-animate.js"></script>
-			<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.6.1/angular-sanitize.js"></script>
-			<script src="//angular-ui.github.io/bootstrap/ui-bootstrap-tpls-2.5.0.js"></script>
+			<script src="{{ asset('/client/js/angular.js') }}"></script>
+			<script src="{{ asset('/client/js/angular-animate.js') }}"></script>
+			<script src="{{ asset('/client/js/angular-sanitize.js') }}"></script>
+			<script src="{{ asset('/client/js/ui-bootstrap-tpls-2.5.0.js') }}"></script>
 			<script src="{{ asset('/client/js/menu.js') }}"></script>
 
 		@show
@@ -117,8 +117,8 @@
         </section>
 		
 		@section('footer-javascript')
-			<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
-			<script src="//netdna.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+			<script src="{{ asset('/client/js/jquery-1.11.1.min.js') }}"></script>
+			<script src="{{ asset('/client/js/bootstrap.min.js') }}"></script>
 			<script type="text/javascript" src="{{ asset('/test/main/js/being.js') }}" ></script>
 		@show
     </body>
