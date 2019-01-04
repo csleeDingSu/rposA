91ce9fd4faf90375223b8b91672b3e6fda702885
prem adarsh <premadarsh.it@gmail.com>
2019-01-04 09:36:22 +0800

    Update DispatchJob.php

--- app/Console/Commands/DispatchJob.php
+++ app/Console/Commands/DispatchJob.php
@@ -24,7 +24,7 @@ class DispatchJob extends Command
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Process Game results';
 
     /**
      * Create a new command instance.
@@ -38,80 +38,13 @@ class DispatchJob extends Command
 	
 	 public function fire()
     {
-        //$vars = new MMProcessGameResult();
-		//$vars->handle();
-		 
-		 $this->call('game:gs', [ ]);
-		 //$request = Request::create('generateresult', 'GET');
-        //$this->info(app()['Illuminate\Contracts\Http\Kernel']->handle($request));
-		 
-		//C:\xampp\htdocs\newding\app\Console\Commands 
-		//require app_path() . '/Console/Commands/pros/MMProcessGameResult.php' ;
-		 
-		 //App\Console\Commands\pros\
-		//$vars = new MMProcessGameResult();
-		//$vars = new ProcessGameResulta();
-		 
-		// if (!class_exists('MMProcessGameResult')) {
-			//spl_autoload_unregister('MMProcessGameResult');
-			 //echo 'yes';
-			 //require app_path() . '/Console/Commands/pros/MMProcessGameResult.php' ;
-		//}
-		 
-		 
-		/* $autoloadFuncs = spl_autoload_functions();
-//var_dump($autoloadFuncs);
-
-foreach($autoloadFuncs as $unregisterFunc)
-{
-    spl_autoload_unregister($unregisterFunc);
-	//$this->error($unregisterFunc);
-	//print_r($unregisterFunc);
-	//die();
-	
-}
-		 */
-		 
-		 /*
-		 $loader = require 'vendor/autoload.php';
-		 $loader->unregister();
-		 
-		 
-		 $res = get_declared_classes();
-    $autoloaderClassName = '';
-    foreach ( $res as $className) {
-		//echo $className;
-        if (strpos($className, 'App\Console\Commands\MMProcessGameResult') === 0) {
-            $autoloaderClassName = $className; // ComposerAutoloaderInit323a579f2019d15e328dd7fec58d8284 for me
-			
-			 spl_autoload_unregister($autoloaderClassName);
-			
-            break;
-        }
-    }
-		 $this->error($autoloaderClassName);
-		 
-		 require app_path() . '/Console/Commands/pros/MMProcessGameResult.php' ;
-		 
-		 */
-		//echo $autoloaderClassName; 
-		 
-		// die();
-		//if (!class_exists('ProcessGameResulta')) {
-			//$vars = new ProcessGameResulta();
-		//} 
-	   // $vars = new MMProcessGameResult();
-		//$vars->handle();
-		//$this->call('game:gs', [ ]);
+		// $this->call('game:gs', [ ]);		
     }
 	
-	public function runtask()
+	private function runtask()
 	{
 		$this->comment('Stared:'.'----------'.Carbon::now()->toDateTimeString().'----------');
 		$this->call('game:gs', [ ]);
-
-		
-
 		$this->info(' ');			
 		$this->info('-------------Waiting for next queue----------');
 		$this->info(' ');	
@@ -129,15 +62,13 @@ foreach($autoloadFuncs as $unregisterFunc)
 				return FALSE;
 			}
 			
-			$data = ['last_run'=>Carbon::now(),'status'=>1,'notes'=>'','unix_last_run'=>Carbon::now()->timestamp];
-			
+			$data = ['last_run'=>Carbon::now(),'status'=>1,'notes'=>'','unix_last_run'=>Carbon::now()->timestamp];			
 			
 			\DB::table('cron_manager')
             	->where('cron_name', $cname)
             	->update($data);
 			
-			return TRUE;
-			
+			return TRUE;			
 		}
 		$data  = ['cron_name'=>$cname,'last_run'=>Carbon::now(),'unix_last_run'=>Carbon::now()->timestamp,'status'=>1,'notes'=>'new task created'];
 		$id    = \DB::table('cron_manager')->insertGetId($data);
@@ -154,8 +85,6 @@ foreach($autoloadFuncs as $unregisterFunc)
     public function handle()
     {
         $cname  = 'game_generate_result';
-		 
-		
 		ob_start();
 		$hb = $this->heartbeat();
 		
