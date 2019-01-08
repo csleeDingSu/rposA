82fd76223f89cab954112b2220d03a9b6fc5d8ed
Chin Voon Pin <vpchin@gmail.com>
2019-01-08 18:12:06 +0800

    reverted

--- public/client/js/wheel-vip.js
+++ public/client/js/wheel-vip.js
@@ -1,5 +1,4 @@
 var trigger = false;
-var timerInterval = 0;
 
 $(function () {
     var wechat_status = $('#hidWechatId', window.parent.document).val();
@@ -8,12 +7,6 @@ $(function () {
     if(wechat_status == 0 && wechat_name != null) {
         getToken();
         closeModal();
-
-        ifvisible.on("wakeup", function(){
-            console.log('wakeup');
-            clearInterval(timerInterval);
-            getToken();
-        });
     }
 
 });
@@ -525,7 +518,7 @@ function showProgressBar(bol_show){
 function startTimer(duration, timer, freeze_time, token) {
 
     var trigger_time = freeze_time - 1;
-    timerInterval = setInterval(function () {
+    var timerInterval = setInterval(function () {
         minutes = parseInt(timer / 60, 10);
         seconds = parseInt(timer % 61, 10);
 
