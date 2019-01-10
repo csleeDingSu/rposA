0229e3a721909e16ffdd55df82e750aea82a8c36
csleeDingsu <cslee.dingsu@gmail.com>
2019-01-10 11:18:56 +0800

    Update app.php

--- config/app.php
+++ config/app.php
@@ -67,7 +67,7 @@ return [
 
     // 'timezone' => 'Asia/Kuala_Lumpur',
     // 'timezone' => 'Asia/Shanghai',
-    'timezone' => 'UTC',
+    'timezone' => env('timezone', 'UTC'),
 
     /*
     |--------------------------------------------------------------------------
