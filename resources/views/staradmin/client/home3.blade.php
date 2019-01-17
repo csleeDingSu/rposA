09e7c414acced6dcc6342d7ca89f6ff686f5c2da
prem adarsh <premadarsh.it@gmail.com>
2019-01-17 09:44:04 +0800

    Update home3.blade.php

--- resources/views/staradmin/client/home3.blade.php
+++ resources/views/staradmin/client/home3.blade.php
@@ -107,7 +107,9 @@
 
 						 @if(isset($banner)) 				
 						 @foreach($banner as $bner) 	
-							<img data-lazy="{{env('banner_url', 'https://wabao666.com/banner/') . $bner->banner_image}}"  >					
+						<!--	<img data-lazy="{{env('banner_url', 'https://wabao666.com/banner/') . $bner->banner_image}}"  >		-->
+						
+						<img data-lazy="/banner/{{$bner->banner_image}}"  >	
 						 @endforeach 				
 						 @endif 
 					
