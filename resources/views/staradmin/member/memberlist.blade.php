766839889178dbfa0b963cf2a5b74ee5a12c37b7
prem adarsh <premadarsh.it@gmail.com>
2019-01-18 10:26:23 +0800

    Update memberlist.blade.php

--- resources/views/staradmin/member/memberlist.blade.php
+++ resources/views/staradmin/member/memberlist.blade.php
@@ -158,7 +158,7 @@
 		<div class="modal-dialog modal-lg" role="document">
 			<div class="modal-content">
 				<div class="modal-header">
-					<h5 class="modal-title">@lang('dingsu.add') @lang('dingsu.life') </h5>
+					<h5 class="modal-title">@lang('dingsu.adjust_wallet')</h5>
 					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                           </button>				
