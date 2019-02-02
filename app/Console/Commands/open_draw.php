d60e7230ae4fde857d260955bee5a562d2529645
Prem Adarsh <premadarsh.it@gmail.com>
2019-02-02 11:43:06 +0800

    Update open_draw.php

--- app/Console/Commands/open_draw.php
+++ app/Console/Commands/open_draw.php
@@ -12,6 +12,12 @@ use App\member_game_bet_temp;
 
 use App\member_game_notification;
 use App\Package;
+
+
+use App\Http\Controllers\RedisGameController;
+
+
+
 class open_draw extends Command
 {
     /**
@@ -46,35 +52,49 @@ class open_draw extends Command
     public function handle()
     {
         
-		$this->comment('Stared:'.'----------'.Carbon::now()->toDateTimeString().'----------');		
-		
+		$this->comment('Stared:'.'----------'.Carbon::now()->toDateTimeString().'----------');	
 		
 		$drawid = $this->argument('drawid');
         
-        if ($drawid == '0') $drawid = 1546;
-		
+        if ($drawid == '0') $drawid = 66487;		
 		
 		//$drawid = 5676  ;
 		
-		$draw =  \DB::table('game_result')->where('id', '=', $drawid)->first();
+		$draw =  \DB::table('game_result')->where('id', '=', $drawid)->first();		
 		
-		
-		if (!$draw) dd('unknown draw');
-		
+		if (!$draw) dd('unknown draw');		
+
+		$gameid = $draw->game_id;
+
 		$mers = \DB::table('redis')->select('member_id')->get();
-				
+		$ReportController = new RedisGameController(); 
+        				
 		if ($mers)
 		{			
+			$setting       = \App\Admin::get_setting();
+			$now           = Carbon::now();
+			$latest_result = Game::get_latest_result($draw->game_id);
+			$futureresult  = Game::get_future_result($draw->game_id, $now );
+			$gamesetting   = $ReportController->get_game_setting($draw , $now); 
+			$gamehistory   = $ReportController->get_game_history($draw->game_id);			
+			
 			foreach ($mers as $key => $val)
 			{
-				$now = Carbon::now();
-				$futureresult = Game::get_future_result($draw->game_id, $now );
-				$setting      = \App\Admin::get_setting();
-				$data         = ['drawid'=> $draw->id, 'futureresults' => $futureresult,'wabaofee' => $setting->wabao_fee];
+				$memberid = $val->member_id;
+				$vip = '';
+				$level            =  Game::get_member_current_level($gameid, $memberid, $vip);
+				$consecutive_lose = Game::get_consecutive_lose($memberid,$gameid, $vip);
+				$gamenotific      = $ReportController->get_game_notification($key,$draw->game_id);
+				
+				$data         = ['drawid'=> $draw->id, 'futureresults' => $futureresult,'wabaofee' => $setting->wabao_fee,'setting' => $setting,'latest_result' => $latest_result,'gamesetting' => $gamesetting,'gamehistory' => $gamehistory];
+
+				//print_r($data);die();
 				event(new \App\Events\EventGameSetting($val->member_id,$data)); //@todo:-move to outisde of the loop
+
+				$this->line('yes--');
 			}
 		}
-				
+			
 		$this->line('End:'.'-------------'.Carbon::now()->toDateTimeString().'----------');
 		
 		$this->info('-------------All done----------');
