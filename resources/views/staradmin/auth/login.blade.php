22963f634947c7c58bc5ef4da341176c45a877b0
csleeDingsu <cslee.dingsu@gmail.com>
2019-02-20 12:07:27 +0800

    Update login.blade.php

--- resources/views/staradmin/auth/login.blade.php
+++ resources/views/staradmin/auth/login.blade.php
@@ -87,7 +87,7 @@
             return false;
         }else{
 
-           // window.location.href = "https://www.wabao666.com/register/";
+           window.location.href = "https://www.wabao666.com/" . + window.location.pathname;
 
         }
 
