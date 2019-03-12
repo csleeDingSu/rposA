e4b11e9cbc926bcb0dd588810fd8d624ae472dd4
csleeDIngsu <cslee.dingsu@gmail.com>
2019-03-12 14:19:46 +0800

    update share

--- resources/views/staradmin/client/angpao.blade.php
+++ resources/views/staradmin/client/angpao.blade.php
@@ -1,6 +1,6 @@
-@extends('layouts.default')
+@extends('layouts.default_without_footer')
 
-@section('title', '登录 / 注册')
+@section('title', '好友分享給你')
 
 @section('top-css')
     @parent
@@ -19,7 +19,7 @@
     var href_ = "http://www.wabao777.com" + sNewPathName;
     var bg_ = window.location.origin + "/vwechat/images/share.png";
 
-    document.writeln("<a href='"+href_+"'><div id=\"weixinTips2\" style=\"display:block;background:rgba(255, 255, 255,1);width:100%;height:100%;position:fixed;left:0;top:0;z-index:9999\"><div id=\"weixinTipsImg\" style=\"background:url("+bg_+") top center no-repeat;background-size:100%;width:100%;height:100%\"><\/div><\/div></a>");
+    // document.writeln("<div class=\"loginBox\"><a href='"+href_+"'><div id=\"weixinTips2\" style=\"display:block;background:rgba(255, 255, 255,1);width:100%;height:100%;position:fixed;left:0;top:0;z-index:9999\"><div id=\"weixinTipsImg\" style=\"background:url("+bg_+") top center no-repeat;background-size:100%;width:100%;height:100%\"><\/div><\/div></a><\/div>");
 
 </script>
 @parent
@@ -27,9 +27,11 @@
 @endsection
 
 @section('content')
-@parent
-    <div class="loginBox">
-    	
-                
-    </div>
+<div class="loginBox">
+	<a href='{{str_replace('vregister','vvregister', \Request::url())}}'>
+    	<div id="weixinTips2" style="background:rgba(255, 255, 255,1);width:100%;height:100%;position:fixed;left:0;top:0;z-index:9999">
+    		<div id="weixinTipsImg" style="background:url('/vwechat/images/share.png') top center no-repeat;background-size:100%;width:100%;height:100%"></div>
+    	</div>
+    </a>
+</div>
 @endsection
---
+++ resources/views/staradmin/layouts/default_without_footer.blade.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}" ng-app="arcade-app">
+    <head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+		
+        <title>{{env('APP_NAME')}} - @yield('title')</title>
+		
+		@section('top-css')
+			<link href="{{ asset('/client/bootstrap-3.3.7-dist/css/bootstrap.min.css') }}" rel="stylesheet">
+			<link rel="stylesheet" href="{{ asset('/client/fontawesome-free-5.5.0-web/css/all.css') }}" >
+
+			<link rel="stylesheet" href="{{ asset('/test/main/css/public.css') }}" />
+			<link rel="stylesheet" href="{{ asset('/test/main/css/module.css') }}" />
+			<link rel="stylesheet" href="{{ asset('/test/main/css/style.css') }}" />
+			<link rel="stylesheet" href="{{ asset('/client/css/default.css') }}" />
+			<link rel="stylesheet" href="{{ asset('/client/css/footer.css') }}" />
+			
+		@show
+		
+		@section('top-javascript')
+
+		@show
+    </head>
+    <body>
+        @section('top-navbar')
+            <!-- Static navbar -->
+				<nav id="header" class="navbar navbar-default">
+				  <div class="container-fluid">
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="col-xs-3">
+						@yield('left-menu')
+					</div>
+					<div class="col-xs-6">
+						<div class="navbar-title">
+						  <div class="navbar-brand">@yield('title')</div>
+						</div>
+					</div>
+				
+					<div class="col-xs-3">
+					  	<div class="menu-wrapper">
+					  		@yield('menu')
+					  	</div>
+					</div>
+				  </div>
+				  <!-- /.container-fluid -->
+				</nav>
+			<!-- End Static navbar -->
+        @show
+
+        <section class="cardFull card-flex">
+			<div class="cardBody">
+        		@yield('content')
+			</div>
+
+		</section>
+
+		@section('footer-javascript')
+			<script src="{{ asset('/client/js/jquery-1.11.1.min.js') }}"></script>
+			<script src="{{ asset('/client/bootstrap-3.3.7-dist/js/bootstrap.min.js') }}"></script>
+			<script type="text/javascript" src="{{ asset('/test/main/js/being.js') }}" ></script>
+		@show
+    </body>
+</html>
