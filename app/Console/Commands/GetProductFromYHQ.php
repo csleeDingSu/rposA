c827384bea577393d78ab0ed8609d3d81e87e956
csleeDIngsu <cslee.dingsu@gmail.com>
2019-03-28 14:24:45 +0800

    Update GetProductFromYHQ.php

--- app/Console/Commands/GetProductFromYHQ.php
+++ app/Console/Commands/GetProductFromYHQ.php
@@ -45,20 +45,20 @@ class GetProductFromYHQ extends Command
         $starttime = Carbon::now()->toDateTimeString();
         $this->info('Process Start Time   :'.$starttime);
 
-        //test url - 居家日用
-        $product_category = '居家日用';
-        $url = "http://yhq.cn/index.php?r=l&u=767538&cid=4"; 
+        //test url 
+        $url = "http://yhq.cn"; //"http://yhq.cn/index.php?r=l&u=767538&cid=4"; //居家日用 
         $this->info('URL :'.$url);
 
         //original response
-        $res =  $this->getcurl($url);
-        $this->info('Original response :'.$res);
+        $this->getcurl($url);
+        
+        //$this->info('Original response');
+        //var_dump($res);
 
-/*
         //filter unwanted data
-        $content = $this->filter_content($res);
-        $this->info('Filter unwanted data :'.$content);
-
+        //$content = $this->filter_content($res);
+        //$this->info('Filter unwanted data :'.$content);
+/*
         //update data into database
         $filter = ['voucher_id'=>$voucher_id];
         $params = ['product_name' => '', 'product_picurl' => '', 'product_category' => $product_category, 'product_price' => '', 'voucher_price' => '', 'discount_price' => '', 'voucher_pass' => '', 'pass_access_flag' => 1];
@@ -76,32 +76,48 @@ class GetProductFromYHQ extends Command
     
     private function getcurl($url)
     {        
-        $payload = [];
-        $headers = []; //['Content-Type: application/x-www-form-urlencoded'];
-        $option = []; //['connect_timeout' => 60, 'timeout' => 180];
-        $client = new \GuzzleHttp\Client(['http_errors' => true, 'verify' => false]);
-        $request = $client->get($url, ['headers' => $headers, 'form params' => $payload]);
-        $response = $request->getBody()->getContents();
-        return $response;
+        $client = new \GuzzleHttp\Client();
+        // Send an asynchronous request.
+        $request = new \GuzzleHttp\Psr7\Request('GET', $url);
+        $promise = $client->sendAsync($request)->then(function ($response) {
+            // echo 'I completed! ' . $response->getBody();
+            echo 'Completed! ';
+            $res = $response->getBody();
+            //$this->info($res);
+            $content = $this->filter_content($res);
+            $this->info($content);
+
+        });
+        $promise->wait();
+
+        // $payload = [];
+        // $headers = []; //['Content-Type: application/x-www-form-urlencoded'];
+        // $option = []; //['connect_timeout' => 60, 'timeout' => 180];
+        // // $client = new \GuzzleHttp\Client(['http_errors' => true, 'verify' => false]);
+        // $client = new \GuzzleHttp\Client();
+        // $request = $client->get($url, ['headers' => $headers, 'form params' => $payload]);
+        // // $request = $client->get($url);
+        // $response = $request->getBody()->getContents();
+        // return $response;
     }
     
     private function filter_content($content) 
     {
-        $str  = '<button class="itemCopy"';
-		$arr  = explode($str, $content);
-		$arr  = explode('</button', $arr[1]);	
-		$from = '￥';
-		$to   = '￥';
-		$sub  = substr($arr[0], strpos($arr[0],$from)+strlen($from),strlen($arr[0]));
-		$sub  = substr($sub,0,strpos($sub,$to));
-		$sub  = trim($sub);
-		
-		if ($sub)
-		{
-			$sub = '￥'.$sub.'￥';
-			return $sub;
-		}		
-        return FALSE;
+        //get total page
+        $str  = '<a class="item more" href="javascript:void(0);">...</a>';
+        $arr  = explode($str, $content);
+        if ($arr[1]) {
+            $arr  = explode('<a class="next-page"', $arr[1]);   
+            $from = '">';
+            $to   = '</a>';
+            $sub  = substr($arr[0], strpos($arr[0],$from)+strlen($from),strlen($arr[0]));
+            $sub  = substr($sub,0,strpos($sub,$to));
+            $sub  = trim($sub);    
+        } else {
+            $sub = $content;
+        }
+        
+		return $sub;
     }
 }
 
