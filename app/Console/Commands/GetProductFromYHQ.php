759c6fa570d97b93d4f02a31d19aa00800669238
csleeDIngsu <cslee.dingsu@gmail.com>
2019-03-28 17:32:04 +0800

    update

--- app/Console/Commands/GetProductFromYHQ.php
+++ app/Console/Commands/GetProductFromYHQ.php
@@ -77,22 +77,29 @@ class GetProductFromYHQ extends Command
     
     private function getcurl($url)
     {        
-        $client = new \GuzzleHttp\Client();
-        // Send an asynchronous request.
-        $request = new \GuzzleHttp\Psr7\Request('GET', $url);
-        $promise = $client->sendAsync($request)->then(function ($response) {
-            // echo 'I completed! ' . $response->getBody();
-            echo 'Completed! ';
-            $res = $response->getBody();
-            $this->info($res);
-            $content = $this->filter_content($res);
-            $this->info($content);
+        // $client = new \GuzzleHttp\Client();
+        // // Send an asynchronous request.
+        // $request = new \GuzzleHttp\Psr7\Request('GET', $url);
+        // $promise = $client->sendAsync($request)->then(function ($response) {
+        //     // echo 'I completed! ' . $response->getBody();
+        //     echo 'Completed! ';
+        //     $res = $response->getBody();
+        //     $this->info($res);
+        //     $content = $this->filter_content($res);
+        //     $this->info($content);
 
-        });
-        $promise->wait();
+        // });
+        // $promise->wait();
         
-        // $file = File::get(storage_path('yhq_test.txt'));
-        // $content = $this->filter_content($file);
+        $remote_file_url = "http://localhost:8899/yhq_test.txt";
+        $file = fopen($remote_file_url, "r");
+        $temp = null;
+        while (($line = fgets($file)) !== false) {
+            $temp .= $line;
+          // echo $line;
+        }
+        fclose($file);
+        $content = $this->filter_content($temp);
 
     }
     
@@ -118,12 +125,15 @@ class GetProductFromYHQ extends Command
         $str  = '<div class="list_cent">';
         $arr  = explode($str, $content);
         if (isset($arr[1])) {
+
             //products
-            $arr  = explode('<a target="_blank" class="goods_list', $arr[1]);   
+            $arr  = explode('<a target="_blank" class="goods_list" ', $arr[1]);   
+            var_dump($arr[0]);
             $from = 'href="';
             $to   = '">';
             $sub  = substr($arr[0], strpos($arr[0],$from)+strlen($from),strlen($arr[0]));
             $sub  = substr($sub,0,strpos($sub,$to));
+            var_dump($sub);
             $items[]  = trim($sub);    
         
             // $products = $arr[1];
