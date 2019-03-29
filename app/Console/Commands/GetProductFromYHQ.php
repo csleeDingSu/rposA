61464eea7d87366408723e2302aa626c6cbe5ed5
csleeDIngsu <cslee.dingsu@gmail.com>
2019-03-29 08:20:23 +0800

    Update GetProductFromYHQ.php

--- app/Console/Commands/GetProductFromYHQ.php
+++ app/Console/Commands/GetProductFromYHQ.php
@@ -46,60 +46,60 @@ class GetProductFromYHQ extends Command
         $starttime = Carbon::now()->toDateTimeString();
         $this->info('Process Start Time   :'.$starttime);
 
-        //test url 
+        //url 
         $url = "http://yhq.cn"; //"http://yhq.cn/index.php?r=l&u=767538&cid=4"; //居家日用 
         $this->info('URL :'.$url);
 
-        //original response
+        //response
         $this->getcurl($url);
-        
-        //$this->info('Original response');
-        //var_dump($res);
-
-        //filter unwanted data
-        //$content = $this->filter_content($res);
-        //$this->info('Filter unwanted data :'.$content);
-/*
-        //update data into database
-        $filter = ['voucher_id'=>$voucher_id];
-        $params = ['product_name' => '', 'product_picurl' => '', 'product_category' => $product_category, 'product_price' => '', 'voucher_price' => '', 'discount_price' => '', 'voucher_pass' => '', 'pass_access_flag' => 1];
-        $array = array_merge($filter, $params);
-        $result = vouchers_yhq::updateOrCreate($filter, $array)->id;
-        $this->info('Result ID :'.$result);
-
-        //end
-        $endtime = Carbon::now()->toDateTimeString();
-        $this->info('Process End Time   :'.$endtime);
-        
-        $this->line(' ');        
-*/		
+                        		
     }
     
     private function getcurl($url)
     {        
-        // $client = new \GuzzleHttp\Client();
-        // // Send an asynchronous request.
-        // $request = new \GuzzleHttp\Psr7\Request('GET', $url);
-        // $promise = $client->sendAsync($request)->then(function ($response) {
-        //     // echo 'I completed! ' . $response->getBody();
-        //     echo 'Completed! ';
-        //     $res = $response->getBody();
-        //     $this->info($res);
-        //     $content = $this->filter_content($res);
-        //     $this->info($content);
+        $client = new \GuzzleHttp\Client();
+        // Send an asynchronous request.
+        $request = new \GuzzleHttp\Psr7\Request('GET', $url);
+        $promise = $client->sendAsync($request)->then(function ($response) {
+            // echo 'I completed! ' . $response->getBody();
+            echo 'Completed! ';
+            $res = $response->getBody();
+            // $this->info($res);
+            $content = $this->filter_content($res);
 
-        // });
-        // $promise->wait();
+            if (count($content['items']) > 0) {
+
+                var_dump(json_encode($content['items']));
+
+            } else {
+                $this->warning("do nothing");
+            }
+            
+            // //update data into database
+            // $filter = ['voucher_id'=>$voucher_id];
+            // $params = ['product_name' => '', 'product_picurl' => '', 'product_category' => $product_category, 'product_price' => '', 'voucher_price' => '', 'discount_price' => '', 'voucher_pass' => '', 'pass_access_flag' => 1];
+            // $array = array_merge($filter, $params);
+            // $result = vouchers_yhq::updateOrCreate($filter, $array)->id;
+            // $this->info('Result ID :'.$result);
+
+            // //end
+            // $endtime = Carbon::now()->toDateTimeString();
+            // $this->info('Process End Time   :'.$endtime);
+            
+            // $this->line(' ');
+
+        });
+        $promise->wait();
         
-        $remote_file_url = "http://localhost:8899/yhq_test.txt";
-        $file = fopen($remote_file_url, "r");
-        $temp = null;
-        while (($line = fgets($file)) !== false) {
-            $temp .= $line;
-          // echo $line;
-        }
-        fclose($file);
-        $content = $this->filter_content($temp);
+        // $remote_file_url = "http://localhost:8899/yhq_test.txt";
+        // $file = fopen($remote_file_url, "r");
+        // $temp = null;
+        // while (($line = fgets($file)) !== false) {
+        //     $temp .= $line;
+        //   // echo $line;
+        // }
+        // fclose($file);
+        // $content = $this->filter_content($temp);
 
     }
     
@@ -122,49 +122,82 @@ class GetProductFromYHQ extends Command
         }
 
         //get product list
+
         $str  = '<div class="list_cent">';
         $arr  = explode($str, $content);
+        
         if (isset($arr[1])) {
 
-            //products
-            $arr  = explode('<a target="_blank" class="goods_list" ', $arr[1]);   
-            var_dump($arr[0]);
-            $from = 'href="';
-            $to   = '">';
-            $sub  = substr($arr[0], strpos($arr[0],$from)+strlen($from),strlen($arr[0]));
-            $sub  = substr($sub,0,strpos($sub,$to));
-            var_dump($sub);
-            $items[]  = trim($sub);    
-        
-            // $products = $arr[1];
+            $arr_  = explode('<a target="_blank" class="goods_list" ', $arr[1]);
+            $t = 1;
 
-            // do{
-            //     $_str  = '<a target="_blank" class="goods_list" ';
-            //     $_arr  = explode($_str, $products);
-            //     if ($_arr[1]) {
+            do {
 
-            //         //get product detail url
-            //         $from = 'href="';
-            //         $to   = '">';
-            //         $sub  = substr($_arr[0], strpos($_arr[0],$from)+strlen($from),strlen($_arr[0]));
-            //         $sub  = substr($sub,0,strpos($sub,$to));
-            //         $product_detail_url  = trim($sub);
-            //         $items[] = $product_detail_url;
+                // var_dump($arr_[$t]);
 
-            //         //get next product
-            //         $__str  = '<a target="_blank" class="goods_list" ';
-            //         $products  = explode($__str, $products);
+                $item = [];
 
-            //     } else {
-            //         break;
-            //     }
-            // }while(false);
+                //url               
+                $from = 'href="';
+                $to   = '">';
+                $sub  = substr($arr_[$t], strpos($arr_[$t],$from)+strlen($from),strlen($arr_[$t]));
+                $sub  = substr($sub,0,strpos($sub,$to));
+                $item['url']  = trim($sub);
+                //img 
+                $from = '<img src="';
+                $to   = '" ';
+                $sub  = substr($arr_[$t], strpos($arr_[$t],$from)+strlen($from),strlen($arr_[$t]));
+                $sub  = substr($sub,0,strpos($sub,$to));
+                $item['pic_url']  = trim($sub); 
+                //id
+                $from = '<span data-gid="';
+                $to   = '">';
+                $sub  = substr($arr_[$t], strpos($arr_[$t],$from)+strlen($from),strlen($arr_[$t]));
+                $sub  = substr($sub,0,strpos($sub,$to));
+                $item['voucher_id']  = trim($sub);
+                //title   
+                $from = '</i>';
+                $to   = '</span>';
+                $sub  = substr($arr_[$t], strpos($arr_[$t],$from)+strlen($from),strlen($arr_[$t]));
+                $sub  = substr($sub,0,strpos($sub,$to));
+                $item['title']  = trim($sub);
+                //original price
+                $from = '<span class="fl">';                
+                $to   = '</span>';
+                $sub  = substr($arr_[$t], strpos($arr_[$t],$from)+strlen($from),strlen($arr_[$t]));
+                $sub  = substr($sub,0,strpos($sub,$to));
+                $item['original_price']  = trim(str_replace('</i>','', str_replace('<i>','',str_replace('原价','',$sub))));
+                //sales
+                $from = '<span class="fr">';
+                $to   = '</span>';
+                $sub  = substr($arr_[$t], strpos($arr_[$t],$from)+strlen($from),strlen($arr_[$t]));
+                $sub  = substr($sub,0,strpos($sub,$to));
+                $item['sales']  = trim(str_replace('</i>','', str_replace('<i>','',str_replace('销量','',$sub))));
+                //discount price
+                $from = '<i class="p">';
+                $to   = '</span>';
+                $sub  = substr($arr_[$t], strpos($arr_[$t],$from)+strlen($from),strlen($arr_[$t]));
+                $sub  = substr($sub,0,strpos($sub,$to));
+                $item['discount_price']  = trim(str_replace('<span>','',str_replace('</i>','', str_replace('<i>','',str_replace('￥','',$sub)))));
+                //voucher price
+                $from = '<b class="fr">';
+                $to   = '</span>';
+                $sub  = substr($arr_[$t], strpos($arr_[$t],$from)+strlen($from),strlen($arr_[$t]));
+                $sub  = substr($sub,0,strpos($sub,$to));
+                $item['voucher_price']  = trim(str_replace('</i>','', str_replace('<i>','',str_replace('元券','',$sub))));
+
+                $t += 1;
+
+                $items[] = $item;
+
+                // break;
+
+            }while(isset($arr_[$t]));
+            // }while($t == 2);
                 
         }
 
         $result = ['pages' => $pages, 'items' => $items];
-        var_dump($result);
-        die('dasdsa');
         
 		return $result;
     }
