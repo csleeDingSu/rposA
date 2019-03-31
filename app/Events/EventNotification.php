6aa3484dc9b58981ee7b1684372a68680be69c48
Prem Adarsh <premadarsh.it@gmail.com>
2019-03-31 16:50:34 +0800

    Create EventNotification.php

---
+++ app/Events/EventNotification.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+
+class EventNotification implements ShouldBroadcast
+{
+    use SerializesModels;
+
+    public $data;
+	public $id;
+
+    public function __construct($id,$data = [])
+    {
+      $this->id   = $id;
+	  $this->data = $data;
+    }
+
+    public function broadcastOn()
+    {
+		return ['notification-'. $this->id ];
+    }
+	
+	public function broadcastWith()
+	{
+		//return [ 'data' => \App\Wallet::get_wallet_details($this->id) ];
+	}	
+	
+}
