d3705f544aef8a34c53f44902ca3417163be63f7
prem adarsh <premadarsh.it@gmail.com>
2019-04-06 16:01:04 +0800

    home product popup

--- resources/views/staradmin/client/home3.blade.php
+++ resources/views/staradmin/client/home3.blade.php
@@ -191,7 +191,7 @@
 		<div class="showQuan dflex scaleHide">
 			<div class="inBox">
 				<img id="showIcon" src="{{ asset('/test/main/images/showIcon.png') }}" class="icon">
-				<h2>复制成功后, 打开淘宝APP即可领优惠卷</h2>
+				<h2>点击下面复制按钮，打开淘宝APP领券</h2>
 
 				
 					<h3 id="cut" class="copyvoucher">￥K8454DFGH45H</h3>
