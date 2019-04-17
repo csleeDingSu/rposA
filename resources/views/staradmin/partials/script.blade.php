cc5612e5c2068e666ea7b12748abf40b5f308f56
prem adarsh <premadarsh.it@gmail.com>
2019-04-17 10:10:44 +0800

    Update script.blade.php

--- resources/views/staradmin/partials/script.blade.php
+++ resources/views/staradmin/partials/script.blade.php
@@ -1,3 +1,136 @@
+	
+<script type="text/javascript">	
+	var url  = "{{ env('APP_URL')}}";		
+	var port = "{{ env('REDIS_CLI_PORT'), '6001' }}";
+	
+	$(document).ready(function () {
+        socketIOConnectionUpdate('<span class="text-info">Requesting Token</span>');
+
+        $.ajax({
+            url: '/admintoken'
+        })
+        .fail(function (jqXHR, textStatus, errorThrown) {
+            htm = '<span class="text-warning">Unauthorized.</span>';
+			socketIOConnectionUpdate( htm);
+        })
+        .done(function (result, textStatus, jqXHR) {
+
+			socketIOConnectionUpdate('<span class="text-info">Response from Server</span>');
+
+			var c_url = url + ':' + port;
+			
+			console.log('connecting URL: '+c_url);
+			
+			//Output have userid , token and username 
+			
+			var socket = new io.connect(c_url, {
+                'reconnection': true,
+                'reconnectionDelay': 1000, //1 sec
+                'reconnectionDelayMax' : 5000,
+                'reconnectionAttempts': 2,
+				'transports': ['websocket'],
+				'timeout' : 10000, //1 min
+				'force new connection' : true,
+				 query: 'token='+result.token
+            });
+
+            /* 
+            connect with socket io
+            */
+            socket.on('connect', function () {
+                socketIOConnectionUpdate('<span class="text-info">Connected, Authenticating</span>')
+                console.log('Token: '+result.token);
+				socket.emit('authenticate', {token: result.token});
+            });
+
+            /* 
+            If token authenticated successfully then here will get message 
+            */
+            socket.on('authenticated', function () {
+				htm = '<span class="text-success">Yes.</span>';
+                socketIOConnectionUpdate(htm);
+            });
+
+            /* 
+            If token unauthorized then here will get message 
+            */
+            socket.on('unauthorized', function (data) {
+                socketIOConnectionUpdate('Unauthorized, error msg: ' + data.message);
+            });
+
+            /* 
+            If disconnect socketio then here will get message 
+            */
+            socket.on('disconnect', function () {
+				console.log('disconnect--');
+				htm = '<span class="text-danger">Disconnected.</span>';
+                socketIOConnectionUpdate(htm);
+            });
+			var trr = '';
+			@section('socket')
+			socket.on("importnoti" + ":App\\Events\\EventDynamicChannel", function(data) {
+				$('#divimportnoti').html('');
+				$.each(data.data, function( index, row ) {
+				  console.log(row);					
+					trr = '<tr id=tr_'+row.id+'><td>'+row.id+'</td><td>'+row.created_at+'</td>	<td>'+row.updated_at+'</td>							<td>'+row.file_name+'</td>	<td id=st_'+row.id+'>'+ getstatus(row.status) +'</td></tr>';
+					
+					$('#divimportnoti').append(trr);
+				});
+			 });
+			
+			
+			socket.on("unr-bulkmove" + ":App\\Events\\EventDynamicChannel", function(data) {
+				var process = data.data;
+				console.log(data);
+				console.log(process);
+				if (process == 'yes')
+				{
+					$( "#so_notification" ).append( '<div id="bulkmove_no_div" class="alert alert-fill-danger" role="alert">   <i class="mdi mdi-alert-circle"></i> @lang("dingsu.bulkmove_in_progress")</div>');
+					
+					$( "#so_notification" ).show();
+				}
+				else
+				{
+					$("#bulkmove_no_div").remove();
+				}
+				
+			 });
+			
+			
+			socket.on("unr-bulkdelete" + ":App\\Events\\EventDynamicChannel", function(data) {
+				var process = data.data;
+				console.log(data);
+				console.log(process);
+				if (process == 'yes')
+				{
+					$( "#so_notification" ).append( '<div id="bulkdelete_no_div" class="alert alert-fill-danger" role="alert">   <i class="mdi mdi-alert-circle"></i> @lang("dingsu.buldelete_in_progress")</div>');
+					
+					$( "#so_notification" ).show();
+				}
+				else
+				{
+					$("#bulkdelete_no_div").remove();
+				}
+				
+			 });
+			
+			@show
+        });
+    });
+		
+		
+		
+/* 
+    Function for print connection message
+    */
+    function socketIOConnectionUpdate(str) {
+        $('#socketconnection').html(str);
+    }	
+		
+		
+		socketIOConnectionUpdate('<span class="text-info">Waiting</span>');
+</script>
+
 <script src="{{ asset('staradmin/vendors/js/vendor.bundle.base.js') }}"></script>
   
 <script src="{{ asset('staradmin/vendors/js/vendor.bundle.addons.js') }}"></script>
