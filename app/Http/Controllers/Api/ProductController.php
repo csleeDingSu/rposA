e77f40c634440e1cc28f8223e1a4b8dadc5d7802
prem adarsh <premadarsh.it@gmail.com>
2019-04-18 14:03:18 +0800

    Update ProductController.php

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -213,7 +213,7 @@ class ProductController extends Controller
 			//prepaid
 			case '2':
 				
-				$data = ['package_id'=>$package->id,'created_at'=>$now,'updated_at'=>$now,'member_id'=>$memberid,'redeem_state'=>1,'request_at'=>$now,'used_point'=>0,'package_life'=>$package->package_life,'package_point'=>$package->package_freepoint];
+				$data = ['package_id'=>$package->id,'created_at'=>$now,'updated_at'=>$now,'member_id'=>$memberid,'redeem_state'=>1,'request_at'=>$now,'used_point'=>0,'package_life'=>$package->package_life,'package_point'=>$package->package_freepoint,'ref_note'=>$request->ref_note];
 				
 				$dd = Package::save_vip_package($data);
 				
