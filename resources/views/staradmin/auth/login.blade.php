1014db60390d3490085fff70918839862b6c9b61
csleeDingsu <cslee.dingsu@gmail.com>
2019-04-23 10:18:16 +0800

    update 注册登录图标

--- resources/views/staradmin/auth/login.blade.php
+++ resources/views/staradmin/auth/login.blade.php
@@ -177,8 +177,8 @@
                                 
                                 <li>
                                     <div class="flexSp">
-                                        <!-- <img src="{{ asset('auth/images/telIcon.png') }}"> -->
-                                        <span><i class="fa fa-user fa-2x" style="color: lightgrey; height: 0.5rem;"></i></span>
+                                        <img src="{{ asset('auth/images/ueserIcon.png') }}">
+                                        <!-- <span><i class="fa fa-user fa-2x" style="color: lightgrey; height: 0.5rem;"></i></span> -->
                                         <input name="username" id="username" type="text" placeholder="@lang('dingsu.username')" required maxlength="30" autofocus>
                                     </div>
                                 </li>
