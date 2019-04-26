64c690ed227240ed36bc7988229c2c4da6622f77
csleeDingsu <cslee.dingsu@gmail.com>
2019-04-26 15:43:25 +0800

    update share product

--- resources/views/staradmin/client/share_product_bk2.blade.php
+++ resources/views/staradmin/client/share_product_bk2.blade.php
@@ -59,16 +59,79 @@
 					</div>
 				</a>
 			</li>
-			<li class="dbox">
-				detail
-			</li>
 		</ul>
 		<!-- 领取优惠券  -->
 		<div class="cvoucher" id="cvoucher">
 			领取成功, 打开淘宝APP领优惠券
 		</div>
 	</div>
+	<a href="/">	
+		<img class="titleimg" src="{{ asset('/client/images/share_product_title.png') }}">
+		@if (!empty($item_featured))
+
+			@php ($t = 0)
+
+			@foreach($item_featured as $f)
+				@php ($t += 1)
+
+				@if ($t % 2 != 0)
+
+					<div class="columnval featuredbg marginleft">
+					  	<div class="free"></div>
+					    <img src="<?=str_replace('_160x160.jpg', '', empty($f->product_picurl) ? env('shareproduct_img', 'https://img.alicdn.com/bao/uploaded/i2/4204664043/O1CN01TCTohy1fjjnPv9Pte_!!0-item_pic.jpg') : $f->product_picurl)?>" style="width:100%">
+				    	<div class="dbox1">
+							<span class="featureddetail">
+								<h2>{{empty($f->product_name) ? env('shareproduct_content', '宝宝鞋儿童小熊鞋老爹鞋子2019新款春秋男童运动鞋潮网红鞋女童鞋') : $f->product_name}}</h2>
+								<div class="price1">
+									<h3>
+									<span class="lbl">券后</span>
+									<span class="lbl_cur">￥</span>
+									<span class="price2">{{number_format(empty($f->discount_price) ? env('shareproduct_pricebefore',20) : $f->discount_price,2)}} </span>
+									<span class="price3">
+									￥{{number_format( empty($f->product_price) ? env('shareproduct_priceafter', 55) : $f->product_price,2)}}
+									</span>
+									</h3>
+									
+								</div>
+							</span>							
+						</div>
+						<div class="dbox1 vlink2">
+							到首页领取 >
+						</div>
+					</div>
+					
+				@else
+
+					<div class="columnval featuredbg marginright">
+					  	<div class="free"></div>
+					    <img src="<?=str_replace('_160x160.jpg', '', empty($f->product_picurl) ? env('shareproduct_img', 'https://img.alicdn.com/bao/uploaded/i2/4204664043/O1CN01TCTohy1fjjnPv9Pte_!!0-item_pic.jpg') : $f->product_picurl)?>" style="width:100%">
+				    	<div class="dbox1">
+							<span class="featureddetail">
+								<h2>{{empty($f->product_name) ? env('shareproduct_content', '宝宝鞋儿童小熊鞋老爹鞋子2019新款春秋男童运动鞋潮网红鞋女童鞋') : $f->product_name}}</h2>
+								<div class="price1">
+									<h3>
+									<span class="lbl">券后</span>
+									<span class="lbl_cur">￥</span>
+									<span class="price2">{{number_format(empty($f->discount_price) ? env('shareproduct_pricebefore',20) : $f->discount_price,2)}} </span>
+									<span class="price3">
+									￥{{number_format( empty($f->product_price) ? env('shareproduct_priceafter', 55) : $f->product_price,2)}}
+									</span>
+									</h3>								
+								</div>
+							</span>							
+						</div>
+						<div class="dbox1 vlink2">
+							到首页领取 >
+						</div>
+				  	</div>
+					  
+				@endif
+
+			@endforeach
+
+		@endif
 	
+	</a>
 
 @endsection
 
