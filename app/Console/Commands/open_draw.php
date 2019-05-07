4debd5552570a7bec17ce4cff2f0ed817e7fca07
csleeDingsu <cslee.dingsu@gmail.com>
2019-05-07 16:06:58 +0800

    update open draw + pre open draw

--- app/Console/Commands/open_draw.php
+++ app/Console/Commands/open_draw.php
@@ -2,19 +2,19 @@
 
 namespace App\Console\Commands;
 
-use Illuminate\Console\Command;
-use Carbon\Carbon;
-use App\Game;
-use Validator;
-use App\Wallet;
-use App\member_game_result;
-use App\member_game_bet_temp;
-
-use App\member_game_notification;
-use App\Package;
-
-use App\Http\Controllers\RedisGameController;
 use App\Events\EventGameSetting ;
+use App\Game;
+use App\Http\Controllers\RedisGameController;
+use App\OpenDrawPre;
+use App\Package;
+use App\Wallet;
+use App\member_game_bet_temp;
+use App\member_game_notification;
+use App\member_game_result;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Session;
+use Validator;
 
 class open_draw extends Command
 {
@@ -49,81 +49,82 @@ class open_draw extends Command
      */
     public function handle()
     {
-        
 		$this->comment('Stared:'.'----------'.Carbon::now()->toDateTimeString().'----------');	
 		
 		$drawid = $this->argument('drawid');
-        $now           = Carbon::now();
-        if ($drawid == '0') $drawid   = 6666;		
+  //       $now           = Carbon::now();
+  //       if ($drawid == '0') $drawid   = 6666;		
 		
-		$draw =  \DB::table('game_result')->select('id as result_id','game_id','game_level_id','created_at','expiry_time','game_result')->where('id', '=', $drawid)->first();		
+		// $draw =  \DB::table('game_result')->select('id as result_id','game_id','game_level_id','created_at','expiry_time','game_result')->where('id', '=', $drawid)->first();		
 		
-		if (!$draw) dd('unknown draw');		
-		$this->info('Draw ID :'.'--------'.$drawid.'----------');
-		$ReportController = new RedisGameController(); 
-		$latest_result = Game::get_latest_result($draw->game_id);
-		$gamesetting   = $ReportController->get_game_setting($draw , $now); 
-		event(new \App\Events\EventDynamicChannel('activedraw','',['gamesetting'=>$gamesetting,'latest_result'=>$latest_result]));
+		// if (!$draw) dd('unknown draw');		
+		// $this->info('Draw ID :'.'--------'.$drawid.'----------');
+		// $ReportController = new RedisGameController(); 
+		// $latest_result = Game::get_latest_result($draw->game_id);
+		// $gamesetting   = $ReportController->get_game_setting($draw , $now); 
+		// // event(new \App\Events\EventDynamicChannel('activedraw','',['gamesetting'=>$gamesetting,'latest_result'=>$latest_result]));
 		
-		$gameid = $draw->game_id;
-		$event_data = [];
-		$mers = \DB::table('redis')
-			->join('members', 'members.id', '=', 'redis.member_id')
-			->whereDate('members.updated_at', Carbon::today())
-			->select('redis.member_id')
-			->get();
-		
+		// $gameid = $draw->game_id;
+		// $event_data = [];
+		// $mers = \DB::table('redis')
+		// 	->join('v_oauth_access_tokens', 'v_oauth_access_tokens.user_id', '=', 'redis.member_id')
+		// 	->where('v_oauth_access_tokens.expires_at', '>=', Carbon::now())
+		// 	->select('redis.member_id')
+		// 	->get();		
         				
-		if ($mers)
-		{			
-			$setting       = \App\Admin::get_setting();
-			$now           = Carbon::now();
+		// if ($mers)
+		// {	
+		// 	$setting       = \App\Admin::get_setting();
+		// 	$now           = Carbon::now();
 			
-			//$futureresult  = Game::get_future_result($draw->game_id, $now );
+		// 	//$futureresult  = Game::get_future_result($draw->game_id, $now );
 			
-			$gamehistory   = $ReportController->get_game_history($draw->game_id);			
-			$this->comment('Get Data:'.'--------'.Carbon::now()->toDateTimeString().'----------');	
-			foreach ($mers as $key => $val)
-			{
-				$memberid = $val->member_id;
-				$vip = '';
-				
-				$level            = Game::get_member_current_level($gameid, $memberid, $vip);
-				$consecutive_lose = Game::get_consecutive_lose($memberid,$gameid, $vip);
-				
-				$vip = 'yes';
-				$vip_level        = Game::get_member_current_level($gameid, $memberid, $vip);
-				$vip_con_lose     = Game::get_consecutive_lose($memberid,$gameid, $vip);
-				
-				$gamenotific      = $ReportController->get_game_notification($key,$draw->game_id);
-				/*
-				$gamenotific = '';
-				$consecutive_lose = [];
-				$level = [];
-				$vip_level = [];
-				$vip_con_lose = [];
-				*/
-				
-				$data         = [ 'member'               => $memberid, 
-								  'drawid'               => $draw->result_id, 
-								 // 'futureresults'		 => $futureresult,
-								  'wabaofee' 			 => $setting->wabao_fee,
-								  'latest_result' 		 => $latest_result,
-								  'gamesetting' 		 => $gamesetting,
-								  'gamehistory' 		 => $gamehistory,
-								  'level'				 => $level,
-								  'viplevel' 			 => $vip_level,
-								  'consecutive_lose'     => $consecutive_lose,
-								  'vip_consecutive_lose' => $vip_con_lose
-								];
-				$event_data[$val->member_id] = 	$data ;	
+		// 	$gamehistory   = $ReportController->get_game_history($draw->game_id);			
+		// 	$this->comment('Get Data:'.'--------'.Carbon::now()->toDateTimeString().'----------');	
+		// 	foreach ($mers as $key => $val)
+		// 	{
 
-				$channel[] = 'initsetting-'.$val->member_id;
-				$message[] = $data ;	
-			}
+		// 		var_dump($val->member_id);
+
+		// 		$memberid = $val->member_id;
+		// 		$vip = '';
+				
+		// 		$level            = Game::get_member_current_level($gameid, $memberid, $vip);
+		// 		$consecutive_lose = Game::get_consecutive_lose($memberid,$gameid, $vip);
+				
+		// 		$vip = 'yes';
+		// 		$vip_level        = Game::get_member_current_level($gameid, $memberid, $vip);
+		// 		$vip_con_lose     = Game::get_consecutive_lose($memberid,$gameid, $vip);
+				
+		// 		$gamenotific      = $ReportController->get_game_notification($key,$draw->game_id);
+		// 		/*
+		// 		$gamenotific = '';
+		// 		$consecutive_lose = [];
+		// 		$level = [];
+		// 		$vip_level = [];
+		// 		$vip_con_lose = [];
+		// 		*/
+				
+		// 		$data         = [ 'member'               => $memberid, 
+		// 						  'drawid'               => $draw->result_id, 
+		// 						 // 'futureresults'		 => $futureresult,
+		// 						  'wabaofee' 			 => $setting->wabao_fee,
+		// 						  'latest_result' 		 => $latest_result,
+		// 						  'gamesetting' 		 => $gamesetting,
+		// 						  'gamehistory' 		 => $gamehistory,
+		// 						  'level'				 => $level,
+		// 						  'viplevel' 			 => $vip_level,
+		// 						  'consecutive_lose'     => $consecutive_lose,
+		// 						  'vip_consecutive_lose' => $vip_con_lose
+		// 						];
+		// 		$event_data[$val->member_id] = 	$data ;	
+
+		// 		$channel[] = 'initsetting-'.$val->member_id;
+		// 		$message[] = $data ;	
+		// 	}
 			
-			$this->comment('End fetch Data:'.'--------'.Carbon::now()->toDateTimeString().'----------');	
-		}
+		// 	$this->comment('End fetch Data:'.'--------'.Carbon::now()->toDateTimeString().'----------');	
+		// }
 			
 			
 		//foreach (array_chunk($event_data,500) as $key=>$val) {
@@ -131,17 +132,24 @@ class open_draw extends Command
 		   //die();
 		//}
 
-				
-		foreach (array_chunk($event_data,100) as $keyc=>$event) {
-			foreach ($event as $val)
-			{
-			   //print_r($val);die();
-			   event(new \App\Events\EventGameSetting($val['member'],$val));
-			   $this->line('yes--'.$val['member']);
-			}		   
+		$result =  OpenDrawPre::select('*')->whereNull('deleted_at')->where('draw_id',$drawid)->first();
+
+		if (!empty($result)) {
+
+			$event_data = json_decode($result->event_data,true);
+
+			foreach (array_chunk($event_data,100) as $keyc=>$event) {
+				foreach ($event as $val)
+				{
+				   //print_r($val);die();
+				   event(new \App\Events\EventGameSetting($val['member'],$val));
+				   $this->line('yes--'.$val['member']);
+				}		   
+			}
+
+			$result = OpenDrawPre::where('id', $result->id)->update(['deleted_at' => Carbon::now()->toDateTimeString()]);		
 		}
 		
-
 		$this->line('End:'.'-------------'.Carbon::now()->toDateTimeString().'----------');
 		
 		$this->info('-------------All done----------');
