c79ec94f2c332ef414d017a108296f9869ee04d0
prem adarsh <premadarsh.it@gmail.com>
2019-05-07 10:23:53 +0800

    Update open_draw.php

--- app/Console/Commands/open_draw.php
+++ app/Console/Commands/open_draw.php
@@ -83,7 +83,7 @@ class open_draw extends Command
 			{
 				$memberid = $val->member_id;
 				$vip = '';
-				/*
+				
 				$level            = Game::get_member_current_level($gameid, $memberid, $vip);
 				$consecutive_lose = Game::get_consecutive_lose($memberid,$gameid, $vip);
 				
@@ -92,13 +92,13 @@ class open_draw extends Command
 				$vip_con_lose     = Game::get_consecutive_lose($memberid,$gameid, $vip);
 				
 				$gamenotific      = $ReportController->get_game_notification($key,$draw->game_id);
-				*/
+				/*
 				$gamenotific = '';
 				$consecutive_lose = [];
-					$level = [];
+				$level = [];
 				$vip_level = [];
 				$vip_con_lose = [];
-				
+				*/
 				
 				$data         = [ 'member'               => $memberid, 
 								  'drawid'               => $draw->result_id, 
