a59c4d07a09a40abc2423d14fdc40da6fec82817
csleeDingsu <cslee.dingsu@gmail.com>
2019-05-08 18:00:05 +0800

    update test

--- app/Console/Commands/Bresult.php
+++ app/Console/Commands/Bresult.php
@@ -56,7 +56,7 @@ class Bresult extends Command
 		
 		sleep(1); //fix for last sec betting
 		
-		$bettinglist =  member_game_bet_temp::where('drawid', $drawid)->where('gametype', 1)->get() ;
+		$bettinglist =  v_member_game_bet_temp::where('drawid', $drawid)->where('gametype', 1)->get() ;
 
 		
 		$current_result = Game::get_single_gameresult($drawid);
