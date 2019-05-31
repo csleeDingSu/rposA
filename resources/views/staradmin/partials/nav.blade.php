6703e83ca6552e60a17b489168697482e1b37a67
csleeDingsu <cslee.dingsu@gmail.com>
2019-05-31 10:37:16 +0800

    update locale

--- resources/views/staradmin/partials/nav.blade.php
+++ resources/views/staradmin/partials/nav.blade.php
@@ -104,7 +104,7 @@
               </a>
               
              
-				<a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">@lang('dingsu.signout')</a>
+				<a class="dropdown-item mt-2" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">@lang('dingsu.signout')</a>
 				
 				<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">{{ csrf_field() }}</form>
 				
