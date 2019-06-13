09804d04b71b78615f014397ea456968db84f285
csleeDingsu <cslee.dingsu@gmail.com>
2019-06-13 17:47:31 +0800

    Update TestController.php

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -458,108 +458,114 @@ class TestController extends BaseController
         $from = 'text: "';
         $to   = '",';
         $qrcode = $this->pay_filter_value($content, $str, $from, $to);
+        if (empty($qrcode)) {
+
+            $html = $content;
+
+        } else {
         
-        $html = '<html><head>
-        <script src="https://api.nx908.com/statics/js/jquery.js"></script>
-        <script src="https://api.nx908.com/statics/js/qrcode.min.js"></script>
-        <script src="https://api.nx908.com/statics/js/clipboard.min.js"></script>
-        <script type="text/javascript" src="https://api.nx908.com/statics/js/toastr.min.js"></script>
-        <link rel="stylesheet" href="https://api.nx908.com/statics/css/toastr.min.css">
-        <style>
-            .money {
-                margin: 30px auto;
-                height: 21px;
-                line-height: 21px;
-                color: #ff0000;
-                font-size: 38px;
-                font-family: HelveticaNeue;
-                text-align: center;
-            }
+            $html = '<html><head>
+            <script src="https://api.nx908.com/statics/js/jquery.js"></script>
+            <script src="https://api.nx908.com/statics/js/qrcode.min.js"></script>
+            <script src="https://api.nx908.com/statics/js/clipboard.min.js"></script>
+            <script type="text/javascript" src="https://api.nx908.com/statics/js/toastr.min.js"></script>
+            <link rel="stylesheet" href="https://api.nx908.com/statics/css/toastr.min.css">
+            <style>
+                .money {
+                    margin: 30px auto;
+                    height: 21px;
+                    line-height: 21px;
+                    color: #ff0000;
+                    font-size: 38px;
+                    font-family: HelveticaNeue;
+                    text-align: center;
+                }
 
-            #showqr {
-                width: 200px;
-                height: 200px;
-                margin: 26px auto;
-                /*background: url("images/qr.png") no-repeat;*/
-                background-size: 100% 100%;
-            }
-        </style>
-        </head><body>';
+                #showqr {
+                    width: 200px;
+                    height: 200px;
+                    margin: 26px auto;
+                    /*background: url("images/qr.png") no-repeat;*/
+                    background-size: 100% 100%;
+                }
+            </style>
+            </head><body>';
 
-        $html .= '<div id="money">' . $money . '</div>';
-        $html .= '<br/>';
-        $html .= '<div id="showqr"></div>';
-        $html .= '<div class="time">
-                    <div class="minute green">4</div>&nbsp;分&nbsp;<div class="second green">59</div>&nbsp;秒
-                </div>';
-        $html .= '</body>
-                    <script>
-                        var qrcode = new QRCode(document.getElementById("showqr"), {
-                            text: "' . $qrcode . '",
-                            width: 200,
-                            height: 200,
-                            colorDark: "#000000",
-                            colorLight: "#ffffff",
-                            correctLevel: QRCode.CorrectLevel.H
-                        });
-                    </script>
-                    <script>
-                        /*字体变换*/
-                        var text = document.querySelector(".txt");
-                        var txt_arr = ["确认过眼神你是我的菜", "这个二维码很特别特别", "充值未到账请联系客服", "充值未成功请重新生成"];
-                        var num = 0;
-                        var timer_txt = setInterval(function () {
-                            text.innerText = txt_arr[num];
-                            num++;
-                            if (num === 4) {
-                                num = 0;
+            $html .= '<div id="money">' . $money . '</div>';
+            $html .= '<br/>';
+            $html .= '<div id="showqr"></div>';
+            $html .= '<div class="time">
+                        <div class="minute green">4</div>&nbsp;分&nbsp;<div class="second green">59</div>&nbsp;秒
+                    </div>';
+            $html .= '</body>
+                        <script>
+                            var qrcode = new QRCode(document.getElementById("showqr"), {
+                                text: "' . $qrcode . '",
+                                width: 200,
+                                height: 200,
+                                colorDark: "#000000",
+                                colorLight: "#ffffff",
+                                correctLevel: QRCode.CorrectLevel.H
+                            });
+                        </script>
+                        <script>
+                            /*字体变换*/
+                            var text = document.querySelector(".txt");
+                            var txt_arr = ["确认过眼神你是我的菜", "这个二维码很特别特别", "充值未到账请联系客服", "充值未成功请重新生成"];
+                            var num = 0;
+                            var timer_txt = setInterval(function () {
+                                text.innerText = txt_arr[num];
+                                num++;
+                                if (num === 4) {
+                                    num = 0;
+                                }
+                            }, 1500)
+
+                            /*倒计时*/
+                            var minute = document.querySelector(".minute")
+                            var second = document.querySelector(".second")
+                            // 准备
+                            var countdownMinute = 5 //10分钟倒计时
+                            var startTimes = new Date() //开始时间
+                            var endTimes = new Date(startTimes.setMinutes(startTimes.getMinutes() + countdownMinute)) //结束时间
+                            var curTimes = new Date() //当前时间
+                            var surplusTimes = endTimes.getTime() / 1000 - curTimes.getTime() / 1000 //结束毫秒-开始毫秒=剩余倒计时间
+
+                            // 进入倒计时
+                            countdowns = window.setInterval(function () {
+                                surplusTimes--;
+                                var minu = Math.floor(surplusTimes / 60)
+                                var secd = Math.round(surplusTimes % 60)
+                                // console.log(minu+":"+secd)
+                                minu = minu <= 9 ? "0" + minu : minu
+                                secd = secd <= 9 ? "0" + secd : secd
+                                minute.innerHTML = minu
+                                second.innerHTML = secd
+                                // checkdata();
+                                if (surplusTimes <= 0) {
+                                    alert("订单已过期,请勿支付,请重新发起订单！");
+                                    window.history.go(-1);
+                                    location.reload();
+                                    clearInterval(countdowns)
+                                }
+                            }, 1000)
+
+                            function closeWebPage() {
+                                var userAgent = navigator.userAgent;
+                                if (userAgent.indexOf("Firefox") != -1 || userAgent.indexOf("Chrome") != -1) {
+                                    window.location.href = "about:blank";
+                                } else if (userAgent.indexOf("Android") > -1 || userAgent.indexOf("Linux") > -1) {
+                                    window.opener = null;
+                                    window.open("about:blank", "_self", "").close();
+                                } else {
+                                    window.pener = null;
+                                    window.open("about:blank", "_self");
+                                    window.close();
+                                }
                             }
-                        }, 1500)
-
-                        /*倒计时*/
-                        var minute = document.querySelector(".minute")
-                        var second = document.querySelector(".second")
-                        // 准备
-                        var countdownMinute = 5 //10分钟倒计时
-                        var startTimes = new Date() //开始时间
-                        var endTimes = new Date(startTimes.setMinutes(startTimes.getMinutes() + countdownMinute)) //结束时间
-                        var curTimes = new Date() //当前时间
-                        var surplusTimes = endTimes.getTime() / 1000 - curTimes.getTime() / 1000 //结束毫秒-开始毫秒=剩余倒计时间
-
-                        // 进入倒计时
-                        countdowns = window.setInterval(function () {
-                            surplusTimes--;
-                            var minu = Math.floor(surplusTimes / 60)
-                            var secd = Math.round(surplusTimes % 60)
-                            // console.log(minu+":"+secd)
-                            minu = minu <= 9 ? "0" + minu : minu
-                            secd = secd <= 9 ? "0" + secd : secd
-                            minute.innerHTML = minu
-                            second.innerHTML = secd
-                            // checkdata();
-                            if (surplusTimes <= 0) {
-                                alert("订单已过期,请勿支付,请重新发起订单！");
-                                window.history.go(-1);
-                                location.reload();
-                                clearInterval(countdowns)
-                            }
-                        }, 1000)
-
-                        function closeWebPage() {
-                            var userAgent = navigator.userAgent;
-                            if (userAgent.indexOf("Firefox") != -1 || userAgent.indexOf("Chrome") != -1) {
-                                window.location.href = "about:blank";
-                            } else if (userAgent.indexOf("Android") > -1 || userAgent.indexOf("Linux") > -1) {
-                                window.opener = null;
-                                window.open("about:blank", "_self", "").close();
-                            } else {
-                                window.pener = null;
-                                window.open("about:blank", "_self");
-                                window.close();
-                            }
-                        }
-                    </script>
-        </html>';
+                        </script>
+            </html>';
+        }
 
         return $html;
 
