aab887778747622d08675549ba79c00f1b2c488e
Prem Adarsh <premadarsh.it@gmail.com>
2019-06-29 16:53:49 +0800

    Update list.blade.php

--- resources/views/staradmin/buyproduct/pendinglist/list.blade.php
+++ resources/views/staradmin/buyproduct/pendinglist/list.blade.php
@@ -39,9 +39,9 @@ function confirm_redeem_with_input(id)	{
 					id:  id,
 				},
 				success: function ( result ) {
-					console.log(result.result);
+					//console.log(result.result);
 					swal.close();
-					$('#carddata').html(result.result);
+					//$('#carddata').html(result.result);
 					$('#carddetailmode').modal('show');	
 
 /*
