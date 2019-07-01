2970080298e13dfe2dc5163bf2d68813910f24af
csleeDingsu <cslee.dingsu@gmail.com>
2019-07-01 17:45:28 +0800

    Update PaymentController.php

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use \App\Helpers\MakePayment;
+use \App\helpers\MakePayment;
 use App\Http\Controllers\Api\LedgerController;
 use App\Http\Controllers\Api\ProductController as ApiProductController;
 use App\Http\Controllers\ProductController as ProductController;
