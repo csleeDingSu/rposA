954b18f2f7391705312e1040837b9d63052b0cf1
prem adarsh <premadarsh.it@gmail.com>
2019-07-03 17:18:38 +0800

    Update ajaxlist.blade.php

--- resources/views/staradmin/buyproduct/pendinglist/ajaxlist.blade.php
+++ resources/views/staradmin/buyproduct/pendinglist/ajaxlist.blade.php
@@ -3,7 +3,7 @@
 	<div class="col-lg-12 grid-margin stretch-card">
 		<div class="card">
 			<div class="card-body">
-				<h4 class="card-title">@lang('dingsu.basic') @lang('dingsu.product') @lang('dingsu.pending') @lang('dingsu.list')</h4>
+				<h4 class="card-title">@lang('dingsu.product') @lang('dingsu.pending') @lang('dingsu.list')</h4>
 				<div class="table-responsive">
 					<table class="table table-hover">
 						<thead>
