427f5a8bbb0729e1d352b4840d6dc28663fd6e71
prem adarsh <premadarsh.it@gmail.com>
2019-07-03 10:22:00 +0800

    Update Game.php

--- app/Game.php
+++ app/Game.php
@@ -775,7 +775,7 @@ class Game extends Model
 	{
 		//DB::enableQueryLog(); 
 		$date   = date('Y-m-d');
-		$date   = '2019-06-17';
+		//$date   = '2019-06-17';
 		$result = DB::table('view_game_win_lose_by_date')->where('member_id', $memberid)->where('game_id', $gameid)->where('created_at', $date )->first();
 		//dd(DB::getQueryLog());
 		return $result;
