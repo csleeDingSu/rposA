c67149d6b52047a93f77000ba0ad7ed587ed0d03
prem adarsh <premadarsh.it@gmail.com>
2019-07-03 15:03:01 +0800

    Update render_shipping_detail.blade.php

--- resources/views/staradmin/buyproduct/pendinglist/render_shipping_detail.blade.php
+++ resources/views/staradmin/buyproduct/pendinglist/render_shipping_detail.blade.php
@@ -1,16 +1,11 @@
 @foreach($result as $list)
 
-
-
-
-
-
 	<div class="row">
 		<div class="col-md-6">
 			<div class="form-group row">
 				<label for="receiver_name" class="col-sm-6 col-form-label">@lang('dingsu.receiver_name') <span class="text-danger">*</span></label>
 				<div class="col-sm-6">
-					<input type="text" class="form-control" name="receiver_name" id="receiver_name" required value="{{$list->receiver_name}}">
+					<input type="text" class="form-control" name="receiver_name" id="receiver_name" required value="{{$list->receiver_name}}">					
 				</div>
 			</div>
 		</div>
@@ -18,7 +13,7 @@
 			<div class="form-group row">
 				<label for="contact_number" class="col-sm-6 col-form-label">@lang('dingsu.contact_number') <span class="text-danger">*</span></label>
 				<div class="col-sm-6">
-					<input type="text" class="form-control" name="contact_number" value="{{$list->contact_number}}">
+					<input type="text" class="form-control" name="contact_number" id="contact_number" value="{{$list->contact_number}}">
 				</div>
 			</div>
 		</div>
@@ -26,7 +21,7 @@
 <div class="row">
 		<div class="col-md-6">
 			<div class="form-group row">
-				<label for="alternative_contact_number" class="col-sm-6 col-form-label">@lang('dingsu.alternative') @lang('dingsu.contact_number') <span class="text-danger">*</span></label>
+				<label for="alternative_contact_number" class="col-sm-6 col-form-label">@lang('dingsu.alternative') @lang('dingsu.contact_number') </label>
 				<div class="col-sm-6">
 					<input type="text" class="form-control" name="alternative_contact_number" id="alternative_contact_number" value="{{$list->alternative_contact_number}}"> 
 				</div>
@@ -36,7 +31,7 @@
 			<div class="form-group row">
 				<label for="address" class="col-sm-6 col-form-label">@lang('dingsu.address') <span class="text-danger">*</span></label>
 				<div class="col-sm-6">
-					<textarea name="address" class="form-control" >{{$list->address}}</textarea>
+					<textarea name="address" id="address" class="form-control" >{{$list->address}}</textarea>
 				</div>
 			</div>
 		</div>
@@ -54,7 +49,7 @@
 			<div class="form-group row">
 				<label for="zip" class="col-sm-6 col-form-label">@lang('dingsu.zip') <span class="text-danger">*</span></label>
 				<div class="col-sm-6">
-					<input type="text" class="form-control" name="zip" value="{{$list->zip}}">
+					<input type="text" class="form-control" name="zip" id="zip" value="{{$list->zip}}">
 				</div>
 			</div>
 		</div>
@@ -70,9 +65,9 @@
 		</div>
 		<div class="col-md-6">
 			<div class="form-group row">
-				<label for="notes" class="col-sm-6 col-form-label">@lang('dingsu.notes') <span class="text-danger">*</span></label>
+				<label for="notes" class="col-sm-6 col-form-label">@lang('dingsu.notes') </label>
 				<div class="col-sm-6">
-					<textarea name="notes" class="form-control">{{$list->notes}}</textarea>
+					<textarea name="notes" id="notes" class="form-control">{{$list->notes}}</textarea>
 				</div>
 			</div>
 		</div>
@@ -85,19 +80,18 @@
 				<div class="col-sm-6">
 					<input type="text" class="form-control" name="tracking_number" id="tracking_number" value="{{$list->tracking_number}}">
 				</div>
+				<error></error>
 			</div>
 		</div>
 		<div class="col-md-6">
 			<div class="form-group row">
 				<label for="tracking_partner" class="col-sm-6 col-form-label">@lang('dingsu.tracking_partner') <span class="text-danger">*</span></label>
 				<div class="col-sm-6">
-					<input type="text" class="form-control" name="tracking_partner" value="{{$list->tracking_partner}}">
+					<input type="text" class="form-control" name="tracking_partner" id="tracking_partner" value="{{$list->tracking_partner}}">
 				</div>
 			</div>
 		</div>
 	</div>
 
-
 @endforeach
-
 <input type="hidden" name="rid" id="rid" value="{{$orderid}}">
