e1eedc19fa8642bf3657784133269a9dc979b25d
prem adarsh <premadarsh.it@gmail.com>
2019-07-05 11:14:06 +0800

    Create AmendWallet.php

---
+++ app/Console/Commands/AmendWallet.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Game;
+use App\Http\Controllers\TestController;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class AmendWallet extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'amend:wallet';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'amend wallet point';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $data = \DB::table('mainledger')->where('is_batched',0)->where('current_point','>',9)->get()->toArray();
+		foreach (array_chunk($data,100) as $key=>$value) {
+			foreach ($value as $val)
+			{
+				$opoint = $val->current_point;
+				$npoint = $opoint / 10;
+				\DB::table('mainledger')
+				->where('id', $val->id)
+				->update(['current_point'=>$npoint]);				
+				$this->info($val->member_id.'-- updated point from '.$opoint. ' to '.$npoint);
+			}		   
+		}		
+    }	
+}
+
+
+
+
+
+
+
+
+
+
+
