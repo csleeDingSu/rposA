e983c96c77f2ef0d8b35c22161116814a928161c
csleeDingsu <cslee.dingsu@gmail.com>
2019-07-06 22:06:42 +0800

    update getNumeric

--- public/client/js/redeem.js
+++ public/client/js/redeem.js
@@ -933,10 +933,3 @@ function getVirtualCardDetails(id, token){
         }
      });
 }
-
-function getNumeric(value) {
-    
-    return ((value % 1) > 0) ? Number(parseFloat(value).toFixed(2)) : Number(parseInt(value)) ;
-
-}
-        
