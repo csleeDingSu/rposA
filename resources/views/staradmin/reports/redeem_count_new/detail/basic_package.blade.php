a915363c5fe8b2e0c839bd8b3cdf479036ecc59d
prem adarsh <premadarsh.it@gmail.com>
2019-07-16 10:25:21 +0800

    as

--- resources/views/staradmin/reports/redeem_count_new/detail/basic_package.blade.php
+++ resources/views/staradmin/reports/redeem_count_new/detail/basic_package.blade.php
@@ -1,4 +1,12 @@
-<thead>
+<div class="row">
+	@if(!$result->isEmpty())
+	<div class="col-lg-12 grid-margin stretch-card">
+		<div class="card">
+			<div class="card-body">
+				<h4 class="card-title">@lang('dingsu.redeem') @lang('dingsu.history') @lang('dingsu.list')</h4>
+				<div class="table-responsive">
+					<table class="table table-hover listtable" id="listtable">
+						<thead>
 <tr>
 <th>@lang('dingsu.id')</th>
 <th>@lang('dingsu.create_Date')</th>
@@ -38,3 +46,18 @@
 </tr>
 @endforeach
 	</tbody>			
+</table>
+					{!! $result->render() !!}
+				</div>
+			</div>
+		</div>
+	</div>
+	@else
+	<div class="col-lg-12 grid-margin stretch-card">
+		<div class="card">
+			<h3 class="mt-3 mb-3 text-danger font-weight-medium text-center">
+                     @lang('dingsu.no_record_found') </h>               
+              </div>
+            </div>	
+	@endif
+	</div>
--- resources/views/staradmin/reports/redeem_count_new/detail/buyproduct.blade.php
+++ resources/views/staradmin/reports/redeem_count_new/detail/buyproduct.blade.php
@@ -1,4 +1,11 @@
-<thead>
+<div class="row">
+	@if(!$result->isEmpty())
+	<div class="col-lg-12 grid-margin stretch-card">
+		<div class="card">
+			<div class="card-body">
+				<h4 class="card-title">@lang('dingsu.redeem') @lang('dingsu.history') @lang('dingsu.list')</h4>
+				<div class="table-responsive">
+					<table class="table table-hover listtable" id="listtable"><thead>
 	<tr>
 		<th>@lang('dingsu.id')</th>
 		<th>@lang('dingsu.create_Date')</th>
@@ -60,7 +67,19 @@
 </tr>
 @endforeach
 </tbody>	
-
-{!! $result->render() !!}
-
+</table>
+					{!! $result->render() !!}
+				</div>
+			</div>
+		</div>
+	</div>
+	@else
+	<div class="col-lg-12 grid-margin stretch-card">
+		<div class="card">
+			<h3 class="mt-3 mb-3 text-danger font-weight-medium text-center">
+                     @lang('dingsu.no_record_found') </h>               
+              </div>
+            </div>	
+	@endif
+	</div>
 
