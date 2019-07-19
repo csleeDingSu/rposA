0e649cbc4a096089d9d136cf74bd84a6b0d3dcab
prem adarsh <premadarsh.it@gmail.com>
2019-07-19 13:56:20 +0800

    Update member.blade.php

--- resources/views/staradmin/client/member.blade.php
+++ resources/views/staradmin/client/member.blade.php
@@ -36,7 +36,7 @@
 			<!-- member id -->
 			<div class="card left">
 				<div class="col-xs-6 member-wrapper">
-					<div class="profile-pic">  <img class="profile-img-circle" src="{{ $member->profile_pic }}">  </div>
+					<div class="profile-pic">  <img class="profile-img-circle" src="{{ $member->profile_pic }}"> &nbsp; </div>
 					<div class="name">{{ $member->wechat_name ?? $member->username }} </div>
 					<div style="clear: both;"></div>
 				</div>
