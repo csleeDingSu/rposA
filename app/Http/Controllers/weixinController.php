f9406143e69b71f80d6dc4ec5c06092f09d03817
csleeDingsu <cslee.dingsu@gmail.com>
2019-07-19 16:59:29 +0800

    Update weixinController.php

--- app/Http/Controllers/weixinController.php
+++ app/Http/Controllers/weixinController.php
@@ -301,8 +301,8 @@ class weixinController extends BaseController
 
     public function updateQRCodeResponse($userinfo, $response)
     {
-        \Log::warning(json_decode($response)->ticket);
-        return weixin::where('openid', $userinfo['openid'])->where('nickname', $userinfo['nickname'])->update(['response_qrcode'=>$response]);
+        $response = json_decode($response);
+        return weixin::where('openid', $userinfo['openid'])->where('nickname', $userinfo['nickname'])->update(['ticket' => $response->ticket,'response_qrcode'=>$response]);
        
     }
 
