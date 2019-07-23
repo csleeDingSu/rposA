24d0b4f0246fc354ee2751d7f88b4e7c84fd87e9
csleeDingsu <cslee.dingsu@gmail.com>
2019-07-23 12:46:50 +0800

    Update ClientController.php

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -296,13 +296,7 @@ class ClientController extends BaseController
 		Session::push('sharepic', $data->id);
 		Session::save();
 		
-		//show wechat qrcode
-		$u = Member::get_member(Auth::guard('member')->user()->id);
-		$this->wx = new WX();
-		$url = env('APP_URL') . "/weixin/showqrcode/" . $u->openid;
-		$qrcode = $this->wx->send_curl($url); 
-
-		return view('client/share', ['data'=>$data, 'wechat_qrcode' =>$qrcode]);
+		return view('client/share', ['data'=>$data]);
 	}
 	
 	public function sharetest(Request $request)
