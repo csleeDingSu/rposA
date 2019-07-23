b00e8703cb1bd95e1029450ee5950e2767f42d6f
prem adarsh <premadarsh.it@gmail.com>
2019-07-23 15:49:07 +0800

    Update WeiXin.php

--- app/helpers/WeiXin.php
+++ app/helpers/WeiXin.php
@@ -16,7 +16,7 @@ class WeiXin
             $type = !empty($type) ? $type : (empty($request->input('type')) ? 'snsapi_base' : 'snsapi_userinfo');
             $appid=env('weixinid'); //'你的AppId';
             $refcode = $request->refcode; 
-            $redirect_uri =  urlencode(env('weixinurl') . "/mp/getUserInfo/" . $type . "/" . $domain . '?ref_code=' . $refcode);
+            $redirect_uri =  urlencode(env('weixinurl') . "/mp/getUserInfo/" . $type . "/" . $domain );
             $url ="https://open.weixin.qq.com/connect/oauth2/authorize?appid=$appid&redirect_uri=$redirect_uri&response_type=code&scope=$type&state=1#wechat_redirect"; 
             \Log::info(json_encode(['weixin URL' => $url], true));
             
