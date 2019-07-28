ef51ea600e6b89c74b29b589b868257c74b1208a
csleeDingsu <cslee.dingsu@gmail.com>
2019-07-29 07:15:42 +0800

    update lose modal text

--- resources/views/staradmin/client/game-node.blade.php
+++ resources/views/staradmin/client/game-node.blade.php
@@ -879,7 +879,7 @@
 	<div class="modal fade col-md-12" id="lose-modal" tabindex="-1" role="dialog" aria-labelledby="viewvouchermodellabel" aria-hidden="true" style="background-color: rgba(17, 17, 17, 0.65);">
 		<div class="modal-dialog modal-lg" role="document">
 			<div class="modal-title modal-win-header">
-				<div class="modal-win-title">很遗憾猜错了</div>
+				<div class="modal-win-title">差点抽中。。。再来一次。。。</div>
 				<div class="modal-result">您还有1次机会</div>			
 			</div>
 
@@ -890,7 +890,7 @@
 							<div class="modal-card">
 								<div class="modal-instruction">前5局猜错，<span class="highlight-grey">总亏损57元</span>，根据倍增式玩法，第6局将<span class="highlight-green">押注63元</span>，猜对能获得63元奖励，减去亏损的570还能赚6元。<br /><span class="highlight-red">赚到的元可兑换红包</span></div>
 								<div class="close-win-modal modal-redeem-button">
-									知道了
+									再抽一次
 								</div>												
 							</div>
 						</div>
