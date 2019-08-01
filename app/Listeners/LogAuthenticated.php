a6dea535045a47858b36a8109186f450a10f0e24
prem adarsh <premadarsh.it@gmail.com>
2019-08-01 10:15:14 +0800

    Create LogAuthenticated.php

---
+++ app/Listeners/LogAuthenticated.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Listeners;
+use Illuminate\Auth\Events\Authenticated;
+
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+use Carbon\Carbon;
+
+class LogAuthenticated
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  GenerateVoucher  $event
+     * @return void
+     */
+    public function handle(Authenticated $event)
+    {
+		$user = \Auth::guard('member')->user();
+		
+		if ($user->is_purged_gamelife != 1)
+		{
+			$usedlife = \App\Game::IsFirstLife($user->id);
+						
+			if ($usedlife < 1)
+			{
+				if (Carbon::parse($user->created_at)->lt(Carbon::now()->subDay(1)))
+				{
+					$wallet = \App\Wallet::get_wallet_details($user->id);
+					
+					if (!empty($wallet->life))
+					{
+						//reduce one life 
+						\App\Wallet::update_basic_wallet($user->id,1,0,'PWL','deduct', '.Auto purged 1 life');
+						\Log::debug(json_encode(['purged life' => 1,'phone'=>$user->phone,'wechat_name'=>$user->wechat_name], true));
+					}					
+					//update flag
+					$user->is_purged_gamelife = 1;
+					$user->save();
+				}	
+			}
+			else
+			{
+				//update flag
+				$user->is_purged_gamelife = 1;
+				$user->save();
+			}	
+		}
+	}
+	
+
+
+
+
+}
