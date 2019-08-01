442e4663958370fcdf30eb0224fa204b826f91f5
prem adarsh <premadarsh.it@gmail.com>
2019-08-01 16:47:07 +0800

    Update Members.php

--- app/Members.php
+++ app/Members.php
@@ -240,6 +240,7 @@ class Members extends Model
 	public static function purge_game_life($user)
     {				
 		$msg = 'no changes on life';
+		$gameid = 102;
 		if ($user->is_purged_gamelife != 1)
 		{
 			$usedlife = \App\Game::IsFirstLife($user->id);
@@ -256,7 +257,78 @@ class Members extends Model
 					
 					if (!empty($wallet->life))
 					{
-						//\Log::debug(json_encode(['life' =>$wallet->life,'phone'=>$user->phone,'wechat_name'=>$user->wechat_name], true));
+						$max_po = \Config::get('app.coin_max');
+						
+						if (!$max_po) $max_po = 15;
+						
+						if ($wallet->acupoint < $max_po) 
+						{
+							$wallet->acupoint = 0;
+						}
+						else
+						{
+							$wallet->acupoint = $max_po;
+						}
+						
+						$credit_bal=0;
+						$_bal = 120;
+						
+						$current_life	= $wallet->life-1;
+						$credit        	= 0;
+						$debit        	= $wallet->acupoint; 
+
+
+	// ---------------------Balance--------------------------------------
+						$balance_before		=$wallet->balance;
+						if($balance_before!=$_bal){
+							$credit_bal= $_bal-$wallet->balance;
+						}
+						$current_balance	= $wallet->balance +$credit_bal;
+						$balance_after		= $current_balance;
+						$debit_bal			= 0;
+						$current_level 		= 1;
+						$current_bet 		= $wallet->bet;
+	// ---------------------Point--------------------------------------
+
+						$award_bal_before		= $wallet->acupoint;
+						$award_bal_after		= $award_bal_before-$wallet->acupoint;
+						$award_current_bal		= $award_bal_before-$wallet->acupoint;
+						$current_life_acupoint	= $award_bal_before-$wallet->acupoint;
+
+						$current_point=$wallet->point+ $wallet->acupoint;
+					
+						
+						//ac credit 
+						$crd_credit             = $wallet->acupoint;
+						$crd_debit              = 0; 
+						$crd_bal_before			= $wallet->point;
+						$crd_bal_after			= $crd_bal_before+$wallet->acupoint;
+						$crd_current_bal		= $crd_bal_after;
+						
+						
+						//update main ledger
+						$mainledger = [ 
+							'current_point'         => $current_point,
+							'current_balance'       => $current_balance,
+							'current_life_acupoint' => $wallet->acupoint,
+						];
+						
+						DB::table('mainledger')->
+							where('member_id', $user->id)
+							->update($mainledger);
+						
+						//update point ledger balance
+						\App\Wallet::life_redeem_post_ledgerhistory_bal($user->id,$credit_bal,$debit_bal,$balance_before,$balance_after,$current_balance);
+						//update point ledger history
+						\App\Wallet::life_redeem_post_ledgerhistory_pnt($user->id,$credit,$debit,$award_bal_before,$award_bal_after,$award_current_bal);						
+						//ac credit
+						\App\Wallet::life_redeem_post_ledgerhistory_crd($user->id,$crd_credit,$crd_debit,$crd_bal_before,$crd_bal_after,$crd_current_bal);
+						
+						//reset game level
+						Game::reset_member_game_level($user->id , $gameid);
+						
+						
+						
 						//reduce one life 
 						\App\Wallet::update_basic_wallet($user->id,1,0,'PWL','deduct', '.Auto purged 1 life');
 						\Log::debug(json_encode(['purged life' => 1,'phone'=>$user->phone,'wechat_name'=>$user->wechat_name], true));
