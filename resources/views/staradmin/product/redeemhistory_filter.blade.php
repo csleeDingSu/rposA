7c5541d01dd281a1a4fdfda4605a4d957a71b819
prem adarsh <premadarsh.it@gmail.com>
2019-08-01 15:42:45 +0800

    Update redeemhistory_filter.blade.php

--- resources/views/staradmin/product/redeemhistory_filter.blade.php
+++ resources/views/staradmin/product/redeemhistory_filter.blade.php
@@ -18,7 +18,7 @@
 						<div class="col">
 							<label>@lang('dingsu.phone')</label>
 							<div id="bloodhound">
-								<input type="text" class="form-control typeahead tt-input" name="s_username" id="s_username" placeholder="@lang('dingsu.username')">
+								<input type="text" class="form-control typeahead tt-input" name="s_phone" id="s_phone" placeholder="@lang('dingsu.username')">
 							</div>
 						</div>
 						
