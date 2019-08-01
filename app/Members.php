4f12fd45daf8171bfb723c2f49f6693b0e3115d0
prem adarsh <premadarsh.it@gmail.com>
2019-08-01 14:27:21 +0800

    Update Members.php

--- app/Members.php
+++ app/Members.php
@@ -244,19 +244,19 @@ class Members extends Model
 		{
 			$usedlife = \App\Game::IsFirstLife($user->id);
 			
-			\Log::debug(json_encode(['used life' =>$usedlife,'phone'=>$user->phone,'wechat_name'=>$user->wechat_name], true));
+			//\Log::debug(json_encode(['used life' =>$usedlife,'phone'=>$user->phone,'wechat_name'=>$user->wechat_name], true));
 						
 			if ($usedlife < 1)
 			{
 				if (\Carbon\Carbon::parse($user->created_at)->lt(\Carbon\Carbon::now()->subDay(1)))
 				{
-					\Log::debug(json_encode(['date gt 24 hrs' =>'yes','phone'=>$user->phone,'wechat_name'=>$user->wechat_name], true));
+					//\Log::debug(json_encode(['date gt 24 hrs' =>'yes','phone'=>$user->phone,'wechat_name'=>$user->wechat_name], true));
 					
 					$wallet = \App\Wallet::get_wallet_details($user->id);
 					
 					if (!empty($wallet->life))
 					{
-						\Log::debug(json_encode(['life' =>$wallet->life,'phone'=>$user->phone,'wechat_name'=>$user->wechat_name], true));
+						//\Log::debug(json_encode(['life' =>$wallet->life,'phone'=>$user->phone,'wechat_name'=>$user->wechat_name], true));
 						//reduce one life 
 						\App\Wallet::update_basic_wallet($user->id,1,0,'PWL','deduct', '.Auto purged 1 life');
 						\Log::debug(json_encode(['purged life' => 1,'phone'=>$user->phone,'wechat_name'=>$user->wechat_name], true));
