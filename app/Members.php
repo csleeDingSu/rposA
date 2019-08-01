02f62891825a8da5cb88bf71e4bea28359b6b04d
prem adarsh <premadarsh.it@gmail.com>
2019-08-01 13:56:55 +0800

    Update Members.php

--- app/Members.php
+++ app/Members.php
@@ -236,6 +236,43 @@ class Members extends Model
 		
 	}
 	
+	
+	public function purge_game_life($user)
+    {				
+		if ($user->is_purged_gamelife != 1)
+		{
+			$usedlife = \App\Game::IsFirstLife($user->id);
+			
+			\Log::debug(json_encode(['used life' =>$usedlife,'phone'=>$user->phone,'wechat_name'=>$user->wechat_name], true));
+						
+			if ($usedlife < 1)
+			{
+				if (Carbon::parse($user->created_at)->lt(Carbon::now()->subDay(1)))
+				{
+					\Log::debug(json_encode(['date gt 24 hrs' =>'yes','phone'=>$user->phone,'wechat_name'=>$user->wechat_name], true));
+					
+					$wallet = \App\Wallet::get_wallet_details($user->id);
+					
+					if (!empty($wallet->life))
+					{
+						\Log::debug(json_encode(['life' =>$wallet->life,'phone'=>$user->phone,'wechat_name'=>$user->wechat_name], true));
+						//reduce one life 
+						\App\Wallet::update_basic_wallet($user->id,1,0,'PWL','deduct', '.Auto purged 1 life');
+						\Log::debug(json_encode(['purged life' => 1,'phone'=>$user->phone,'wechat_name'=>$user->wechat_name], true));
+					}					
+					//update flag
+					$user->is_purged_gamelife = 1;
+					$user->save();
+				}	
+			}
+			else
+			{
+				//update flag
+				$user->is_purged_gamelife = 1;
+				$user->save();
+			}	
+		}
+	
 		
 		
 }
