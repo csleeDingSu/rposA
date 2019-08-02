3261bb00cf9257672e2cead2cfcf96bd4c3a1e9e
prem adarsh <premadarsh.it@gmail.com>
2019-08-02 15:33:32 +0800

    filter all

--- resources/views/staradmin/reports/play/filter.blade.php
+++ resources/views/staradmin/reports/play/filter.blade.php
@@ -12,7 +12,7 @@
 						<div class="col">
 							<label>@lang('dingsu.drawid')</label>
 							<div id="the-basics">
-								
+								<input type="text" class="form-control typeahead tt-input" name="s_drawid" id="s_drawid" placeholder="@lang('dingsu.drawid')">
 								<select id="s_drawid" name="s_drawid" class="form-control">
 									<option value="" >@lang('dingsu.default_select')</option>
 									<option value="103" selected>103</option>
@@ -28,7 +28,12 @@
 						<div class="col">
 							<label>@lang('dingsu.gameid')</label>
 							<div id="the-basics">
-								<input type="text" class="form-control typeahead tt-input" name="s_gameid" id="s_gameid" placeholder="@lang('dingsu.gameid')">
+								<select id="s_gameid" name="s_gameid" class="form-control">
+									<option value="" >@lang('dingsu.default_select')</option>
+									<option value="103" selected>103</option>
+									<option value="102">102</option>
+									<option value="101">101</option>
+								</select>
 							</div>
 						</div>
 						
