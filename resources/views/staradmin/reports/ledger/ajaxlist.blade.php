bd0a6e2658fdc91578ab920244a8b46d42e4922a
prem adarsh <premadarsh.it@gmail.com>
2019-08-02 14:54:21 +0800

    Update ajaxlist.blade.php

--- resources/views/staradmin/reports/ledger/ajaxlist.blade.php
+++ resources/views/staradmin/reports/ledger/ajaxlist.blade.php
@@ -24,8 +24,11 @@
 								
 								
 	<td>{{ $list->created_at }}        </td>
-	<td> </td>
-								<td><button type="button" data-type="{{ $list->type }}" data-date="{{$list->created_at}}" data-id="{{$list->member_id}}" class="btn  ShowMember"> {{ $list->username }} </button>
+	<td>
+									<img class="profile-img-circle" src="{{ $list->profile_pic ?? '/client/images/avatar.png' }}">&nbsp;
+									{{ $list->wechat_name ?? $list->username }}
+								</td>
+								<td><button type="button" data-type="{{ $list->type }}" data-date="{{$list->created_at}}" data-id="{{$list->member_id}}" class="btn  ShowMember"> {{ $list->phone }} </button>
 								    </td>
 	<td>{{ $list->type }}</td>
 	<td>{{ $list->pname }}</td>
