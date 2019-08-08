c5f230dcc76f25006193d1a4144e6dc1b867a90c
prem adarsh <premadarsh.it@gmail.com>
2019-08-08 11:08:37 +0800

    Update intromodel.blade.php

--- resources/views/staradmin/client/intromodel.blade.php
+++ resources/views/staradmin/client/intromodel.blade.php
@@ -19,7 +19,7 @@
 						</div>
 						<div class="row">
 							<!-- <button class="sec_reg_btn" type="button">没有帐号，去注册</button>	 -->
-							<a class="ssec_reg_btn" data-url="intent://dev.boge56.com/member/login/register"  href="intent://dev.boge56.com/member/login/register" target="_blank">没有帐号，去注册</a>
+							<a class="ssec_reg_btn" data-url="http://dev.boge56.com/member/login/register"  href="http://dev.boge56.com/member/login/register" target="_blank" rel="nofollow">没有帐号，去注册</a>
 						
 						</div>
 						
