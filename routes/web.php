6874b6005be18a7b0964b64a3272bc366547ba58
csleeDingsu <cslee.dingsu@gmail.com>
2019-08-09 18:11:03 +0800

    udpate button-setting

--- routes/web.php
+++ routes/web.php
@@ -110,9 +110,8 @@ Route::group( [ 'middleware' => 'sso' ], function () {
 
 	Route::get( '/faq', function () {
 
-		// $faqs = DB::table( 'faq' )->select( 'id', 'title', 'content' )->orderBy( 'id', 'desc' )->get();
-		$faqs = DB::table( 'faq' )->select( 'id', 'title', 'content' )->orderBy( 'id', 'acs' )->get();
-
+		$faqs = DB::table( 'faq' )->select( 'id', 'title', 'content' )->orderBy( 'id', 'desc' )->get();
+		
 		return view( 'client/faq', compact( 'faqs' ) );
 	} );
 
