2cb1de0b3b64770492a971c1eeeadf92fe7d7421
prem adarsh <premadarsh.it@gmail.com>
2019-08-20 11:32:04 +0800

    Create History.php

---
+++ app/History.php
@@ -0,0 +1,32 @@
+<?php
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+use App\Support\Filterable;
+use Carbon\Carbon;
+
+
+
+class History extends Model
+{   
+    protected $fillable = [        'member_id','credit_type','current_balance','before_life','current_life','debit','credit','notes','balance_before','balance_after','ledger_type','uuid','before_vip_life','current_vip_life','before_vip_point','current_vip_point','ref_id','ref_type','','','',''
+    ];	
+	
+    protected $table = 'ledger_history';	
+	
+	public static function add_ledger_history($data)
+	{
+		$history = new History();	
+		$history->fill($data);
+		$history->uuid = $uuid = unique_numeric_random((new static)->getTable(), 'uuid', 15);			
+		$history->save();
+		return $uuid;
+	}
+	
+	
+}
+
+
+
+
+
+
