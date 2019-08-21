75b3918a0a94c0e7ec52958b36b008e83381f940
prem adarsh <premadarsh.it@gmail.com>
2019-08-21 12:05:21 +0800

    Update MemberController.php

--- app/Http/Controllers/Api/MemberController.php
+++ app/Http/Controllers/Api/MemberController.php
@@ -24,7 +24,7 @@ class MemberController extends Controller
 			$request->type = 'basicpackage';
 		}
 		$result  = \App\History::get_summary($request->memberid,$request->type);
-		$ref_cre = \DB::table('ref_credit_type')->select('name','value')->get();
+		$ref_cre = \DB::table('ref_credit_type')->select('name','value','type')->get();
 		return response()->json(['success' => true,'records'=>$result,'credit_type_ref'=>$ref_cre]); 
 	}
 	public function update_profile()
