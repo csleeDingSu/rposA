598d17e39a121d0e0b148540a73f0b15ea87261b
prem adarsh <premadarsh.it@gmail.com>
2019-09-06 12:11:42 +0800

    Update Ledger.php

--- app/Ledger.php
+++ app/Ledger.php
@@ -31,6 +31,14 @@ class Ledger extends Model
 	//add reserve point
 	reserve($userid,$gameid,$credit = 0,$category = 'PNT', $notes = FALSE)		
 	*/	
+	
+	
+	public static function mainledger($memberid)
+	{
+		return $result = DB::table('mainledger')->select('play_count','current_balance as balance','current_point as point', 'current_level as level', 'current_life as life','current_betting as bet','vip_life','vip_point'
+			, DB::raw('(case when current_life_acupoint is null then 0 else current_life_acupoint end) as acupoint')
+			)->where('member_id', $memberid)->first();
+	}
 	public static function ledger($userid,$gameid)
 	{
 		$wallet     = Ledger::where('member_id',$userid)->where('game_id',$gameid)->first();		
