f2c8aa1272a368ca1e663314922220a82d51db56
Prem Adarsh <premadarsh.it@gmail.com>
2019-09-12 11:01:31 +0800

    Update ajaxlist.blade.php

--- resources/views/staradmin/tabaocron/ajaxlist.blade.php
+++ resources/views/staradmin/tabaocron/ajaxlist.blade.php
@@ -3,7 +3,7 @@
 	<div class="col-lg-12 grid-margin stretch-card">
 		<div class="card">
 			<div class="card-body">
-				<h4 class="card-title">@lang('dingsu.cron') @lang('dingsu.list')</h4>
+				<h4 class="card-title">@lang('dingsu.tabao') @lang('dingsu.cron')</h4>
 				<div class="table-responsive">
 					<table class="table table-hover listtable" id="listtable">
 						<thead>
