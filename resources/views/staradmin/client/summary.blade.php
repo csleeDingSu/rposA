ff023ba4a9782e060fba104cec16304e1d9db4ce
csleeDingsu <cslee.dingsu@gmail.com>
2019-09-17 11:06:06 +0800

    Update summary.blade.php

--- resources/views/staradmin/client/summary.blade.php
+++ resources/views/staradmin/client/summary.blade.php
@@ -1,12 +1,12 @@
-@extends('layouts.default')
+@php
+    if (env('THISVIPAPP','false')) {
+        $default = 'layouts.default_app';
+    } else {
+        $default = 'layouts.default';
+    }
+@endphp
 
-@section('title', '明细')
-
-@section('left-menu')
-    <a href="/profile" class="back">
-        <div class="icon-back glyphicon glyphicon-menu-left" aria-hidden="true"></div>
-    </a>
-@endsection
+@extends($default)
 
 @section('top-css')
     @parent
@@ -16,20 +16,46 @@
 
 @section('top-javascript')
 	@parent
-	<script src="{{ asset('/client/ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js') }}"></script>
+	<!-- <script src="{{ asset('/client/ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js') }}"></script> -->
 @endsection
 
+@if(env('THISVIPAPP','false'))
+    <!-- top nav -->
+    @section('left-menu')
+      <a class="returnBtn" href="javascript:history.back();"><img src="{{ asset('clientapp/images/returnIcon.png') }}"><span>返回</span></a>
+    @endsection
+
+    @section('title', '明细')
+
+    @section('right-menu')
+    @endsection
+    <!-- top nav end-->
+
+@else
+    @section('title', '明细')
+    @section('top-navbar')
+    @endsection
+@endif
+
 @section('content')
+
+@if(!env('THISVIPAPP','false'))
 <div class="full-height">
 	<div class="container">
+@endif
+
 		<input id="hidUserId" type="hidden" value="{{isset(Auth::Guard('member')->user()->id) ? Auth::Guard('member')->user()->id : 0}}" />
 		<input id="hidSession" type="hidden" value="{{isset(Auth::Guard('member')->user()->active_session) ? Auth::Guard('member')->user()->active_session : null}}" />
 		<input id="hidUsername" type="hidden" value="{{isset(Auth::Guard('member')->user()->username) ? Auth::Guard('member')->user()->username : null}}" />
 		<input type="hidden" id="this_vip_app" value="{{ env('THISVIPAPP','false') }}" />
 		<div id="summary"></div>
         <div id="pagination"></div>
+
+@if(!env('THISVIPAPP','false'))
 	</div>
 </div>
+@endif
+
 @endsection
 
 @section('footer-javascript')
