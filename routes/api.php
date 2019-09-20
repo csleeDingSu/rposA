d25436dcdc64e2b68239247a265e64eda53020c6
prem adarsh <premadarsh.it@gmail.com>
2019-09-20 15:06:59 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -167,6 +167,9 @@ Route::group(['namespace' => 'Api'],function()
 	Route::post('/convert-bonuspoint', 'LedgerController@convertbonustolife')->name('convertbonustolife');
 	
 	
+	Route::any('/invitation-list', 'MemberController@invitation_list')->name('invitation_list');
+	
+	
 });
 //Route::post( 'firsttime-login', 'Auth\MemberLoginController@apilogin' )->name( 'api_apilogin' );
 Route::post('api-login', 'Auth\MemberLoginController@apilogin')->name('api_apilogin');
