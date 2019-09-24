1eeee46b6185bbd09a7964940895ad35fdcf987a
csleeDingsu <cslee.dingsu@gmail.com>
2019-09-24 15:29:37 +0800

    Update tabao_product_detail.blade.php

--- resources/views/staradmin/client/tabao_product_detail.blade.php
+++ resources/views/staradmin/client/tabao_product_detail.blade.php
@@ -87,7 +87,8 @@
 						@php ($_url = $data['couponLink'])
 						{{-- @php ($_url = (str_replace('https://','tabao://',$_url))) --}}
 						{{-- @php ($_url = (str_replace('http://','tabao://',$_url))) --}}
-						<a href="{{$_url}}"> 
+						 <a href="taobao://item.taobao.com/item.htm?id=534300346025">淘宝</a>
+						<a href="taobao://item.taobao.com/item.htm?id={{$data['id']}}"> 
 							<div id="btn-copy" class="btn-copy">领取优惠券</div>
 						</a>
 						@endif
