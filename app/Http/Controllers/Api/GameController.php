2e212449586a5fd408ffc91321a106cadfb971a7
prem adarsh <premadarsh.it@gmail.com>
2019-09-24 09:12:45 +0800

    Update GameController.php

--- app/Http/Controllers/Api/GameController.php
+++ app/Http/Controllers/Api/GameController.php
@@ -1828,7 +1828,12 @@ class GameController extends Controller
 	
 	public function usedpoint(Request $request)
     {
-		$point = \DB::table('a_view_used_point')->where('member_id',$request->memberidd)->where('game_id',$request->gameid)->sum('point');
+		$row   = \DB::table('a_view_used_point')->where('member_id',$request->memberidd)->where('game_id',$request->gameid)->first();
+		$point = 0;
+		if ($row) 
+		{
+			$point = $point->point;
+		}
 		return response()->json(['success' => true, 'point' => $point]); 
 	}
 	
