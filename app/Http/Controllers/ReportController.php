ee0163ceeb177d24a28cf2231bd879cefbb9cb79
Prem Adarsh <premadarsh.it@gmail.com>
2019-09-28 10:15:02 +0800

    Update ReportController.php

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -651,7 +651,7 @@ class ReportController extends BaseController
 				->whereHas('ledger', function($q) use($input) {
 					if (!empty($input['s_game']))  
 						$q->where('game_id', $input['s_game']) ;
-					if ($input['s_type'] != '' )
+					if (!empty($input['s_type'] ) )
 						$q->where('ledger_type','=',$input['s_type']);
 				});
 		
