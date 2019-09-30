ee7aeb6a492a8060ebebadc499b03e31b734dd82
Prem Adarsh <premadarsh.it@gmail.com>
2019-09-30 09:37:28 +0800

    Update footer_app.blade.php

--- resources/views/staradmin/layouts/footer_app.blade.php
+++ resources/views/staradmin/layouts/footer_app.blade.php
@@ -71,3 +71,29 @@
         }   
       </style>
     @endif
+
+
+    <script type="javascript">
+    	
+(function(){
+
+  // Really basic check for the ios platform
+  // https://stackoverflow.com/questions/9038625/detect-if-device-is-ios
+  var iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+
+  // Get the device pixel ratio
+  var ratio = window.devicePixelRatio || 1;
+
+  // Define the users device screen dimensions
+  var screen = {
+    width : window.screen.width * ratio,
+    height : window.screen.height * ratio
+  };
+
+  // iPhone X Detection
+  if (iOS && screen.width == 1125 && screen.height === 2436) {
+    $('.card-bar').css({"margin-bottom":"3px"});
+  }
+})();
+
+    </script>
