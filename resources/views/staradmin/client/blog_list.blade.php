9b3a3ee910b3a7a97ec221a12db55dd78c9a319d
csleeDingsu <cslee.dingsu@gmail.com>
2019-10-09 18:08:34 +0800

    update blog

--- resources/views/staradmin/client/blog_list.blade.php
+++ resources/views/staradmin/client/blog_list.blade.php
@@ -14,46 +14,48 @@
                 @foreach(json_decode($b->uploads) as $photo)
                   @php ($i++)
                   @if ($i <= 2)
-                    <li>
-                      <div class="_container">
-                        <div class="_content">
-                          <img class="lazy" src="{{ $photo }}">
-                        </div>
+                  <li>
+                    <div class="_container">
+                      <div class="_content">
+                        <img class="lazy" src="{{ $photo }}">
                       </div>
-                    </li>
-                  @else
-                    
-                    @if ($i > 2)                      
-                      <li style="display: none;">
+                    </div>
+                  </li>
+                  @endif                  
+                @endforeach 
+                
+                @php ($_i = 0)
+                @if ($i > 2)
+                  <li><div class='_container'>
+                  @foreach(json_decode($b->uploads) as $photo)
+                    @php ($_i++)
+                    @if ($_i > 2)
+                      @if ($i > 3)                    
+                        @php($clss = "_content2 pos" .$_i)   
+                        <div class="{{$clss}}"><img class="lazy" src="{{$photo}}"></div>                    
+                      @else
+                        <div class="_content"><img class="lazy" src="{{$photo}}"></div>
+                      @endif
+                    @endif
+                  @endforeach 
+                  </div></li> 
+
+                  @if ($i > 3)
+                    @php ($__i = 0)
+                    @foreach(json_decode($b->uploads) as $photo)
+                      @php ($__i++)
+                      @if ($__i > 3)
+                      <li class="hide">
                         <div class="_container">
                           <div class="_content">
                             <img class="lazy" src="{{ $photo }}">
                           </div>
                         </div>
                       </li>
-                    @endif
-
+                      @endif
+                    @endforeach
                   @endif
-                @endforeach 
-
-                @php ($i = 0)
-                @foreach(json_decode($b->uploads) as $photo)
-                  @php ($i++)
-                    @if ($i >= 3)                      
-                      @if ($i == 3)
-                        <li>
-                          <div class="_container">
-                        @endif                 
-                            @php($clss = "_content2 pos" .$i)   
-                            <div class="{{$clss}}">
-                              <img class="lazy" src="{{ $photo }}">
-                            </div>
-                        @if ($i == 6)
-                          </div>
-                        </li>
-                        @endif
-                    @endif
-                @endforeach  
+              @endif
             @endif
           </ul>
         </div>        
