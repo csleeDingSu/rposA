2d0ced99fae8b8407289079ffc760d5b794cd1a1
csleeDingsu <cslee.dingsu@gmail.com>
2019-10-23 00:35:17 +0800

    Update zeroPricePage.js

--- public/clientapp/js/zeroPricePage.js
+++ public/clientapp/js/zeroPricePage.js
@@ -118,7 +118,7 @@ function populateData(item) {
   commissionRate = (commissionRate <= 0) ? 0 : commissionRate;
   reward = parseInt(Number(promoPrice) * Number(commissionRate));
   reward = (reward <= 0) ? '100' : reward;
-  _param = '?id=' + item.id + '&goodsId='+ item.goodsId +'&mainPic='+item.mainPic+'&title='+item.title+'&monthSales=' + item.monthSales +'&originalPrice=' +oldPrice+'&couponPrice=' +item.couponPrice + '&couponLink=' + encodeURIComponent(item.couponLink) + '&commissionRate=' + commissionRate + '&voucher_pass=';
+  _param = '?id=' + item.id + '&goodsId='+ item.goodsId +'&mainPic='+item.mainPic+'&title='+item.title+'&monthSales=' + item.monthSales +'&originalPrice=' +oldPrice+'&couponPrice=' +item.couponPrice + '&couponLink=' + encodeURIComponent(item.couponLink) + '&commissionRate=' + commissionRate + '&voucher_pass=&life=' + life;
   // _param = '?id=' + item.id + '&goodsId='+ item.goodsId;
 
   html = '<div class="inBox">' +
