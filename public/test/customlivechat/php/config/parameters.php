44c3300b3c04ff4dacacfa86567c341ad7582723
csleeDingsu <cslee.dingsu@gmail.com>
2019-10-31 17:15:35 +0800

    update request

--- public/test/customlivechat/php/config/parameters.php
+++ public/test/customlivechat/php/config/parameters.php
@@ -6,7 +6,7 @@ return array (
   'dbUser' => 'dev1',
   'dbPassword' => 'Abc@1234Abc@1234',
   'dbName' => 'livechat',
-  'superUser' => 'admin',
+  'superUser' => 'admin@admin.com',
   'superPass' => '123456',
   'services' => 
   array (
