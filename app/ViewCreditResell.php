c05d2c802a76d82b4c2b1a8ec2efb52ee48735cd
prem adarsh <premadarsh.it@gmail.com>
2019-10-31 14:24:07 +0800

    Update ViewCreditResell.php

--- app/ViewCreditResell.php
+++ app/ViewCreditResell.php
@@ -3,7 +3,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Carbon\Carbon;
 
-class CreditResell extends Model
+class ViewCreditResell extends Model
 {   
     protected $fillable = [    ];	
 			
