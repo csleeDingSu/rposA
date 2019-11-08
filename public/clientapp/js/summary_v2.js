27d590546e6bc07ccb4f79c0c21bbe3abaa1e2e7
csleeDingsu <cslee.dingsu@gmail.com>
2019-11-08 12:27:54 +0800

    Update summary_v2.js

--- public/clientapp/js/summary_v2.js
+++ public/clientapp/js/summary_v2.js
@@ -375,8 +375,10 @@ function Recharge_html(item){
     html += '<a class="inBox '+_cls+'">';
   }
   
+  console.log(txt_point);
+  console.log((Number(txt_point) >= 100000) && (countdown != ''));
   if ((Number(txt_point) >= 100000) && (countdown != '')) {
-    html += '<h2><span style="font-size: 14px !important">充值' +parseInt(txt_point)+ '挖宝币</span>';
+    html += '<h2><span style="font-size: 14px !important;">充值' +parseInt(txt_point)+ '挖宝币</span>';
   } else {
     html += '<h2><span>充值' +parseInt(txt_point)+ '挖宝币</span>';
   }
