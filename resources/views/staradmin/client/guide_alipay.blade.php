d2b2c30c2c39b949e8de74d0e886c0e468627d8d
csleeDingsu <cslee.dingsu@gmail.com>
2019-11-08 13:38:49 +0800

    udpate reuqest

--- resources/views/staradmin/client/guide_alipay.blade.php
+++ resources/views/staradmin/client/guide_alipay.blade.php
@@ -29,17 +29,20 @@
     <img src="{{ asset('clientapp/images/help/alipay1b.png') }}">
   </dd>
   <dt>
-    <font color="#4eb6ff">02</font><span>显示卖家支付宝账号，点击“去转账>”</span>
+    <font color="#4eb6ff">02</font><span>显示卖家“AA收款账单”吱口令，点击吱口令查看收款金额。确认付款金额后，点击“去支付>”</span>
   </dt>
   <dd>
-    <img src="{{ asset('clientapp/images/help/alipay2.png') }}">
+    <img src="{{ asset('clientapp/images/help/alipay2a.png') }}">
+    <img src="{{ asset('clientapp/images/help/alipay2b.png') }}">
   </dd>
   <dt>
-    <font color="#4eb6ff">03</font><span>输入付款金额，确认转账，完成转账后，进入挖宝平台点击“充值完成”</span>
+    <font color="#4eb6ff">03</font><span>确认付款金额，点击“立即支付”，完成转账后，进入挖宝平台点击“充值完成”</span>
   </dt>
   <dd>
     <img src="{{ asset('clientapp/images/help/alipay3a.png') }}">
     <img src="{{ asset('clientapp/images/help/alipay3b.png') }}">
+    <img src="{{ asset('clientapp/images/help/alipay3c.png') }}">
+    <img src="{{ asset('clientapp/images/help/alipay3d.png') }}">
   </dd>
 </dl>
 @endsection
