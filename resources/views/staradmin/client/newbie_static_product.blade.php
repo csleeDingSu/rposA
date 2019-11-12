2d7dd883ae4c9dbf816a04e66c672a7f651f8e5b
csleeDingsu <cslee.dingsu@gmail.com>
2019-11-12 14:46:58 +0800

    update request

--- resources/views/staradmin/client/newbie_static_product.blade.php
+++ resources/views/staradmin/client/newbie_static_product.blade.php
@@ -1,16 +1,15 @@
+
 <div class="inBox">
   <div class="imgBox">
-    <a href="/main/product/detail?id=260&amp;goodsId=590084304040&amp;mainPic=https://img.alicdn.com/imgextra/i4/2863069160/O1CN018KR3Nx2HXKUI4LwSK_!!2863069160.jpg&amp;title=垃圾袋家用手提式加厚黑色宿舍用学生大号拉圾塑料袋抽绳自动收口&amp;monthSales=1188752&amp;originalPrice=6.9&amp;couponPrice=1&amp;couponLink=https%3A%2F%2Fuland.taobao.com%2Fquan%2Fdetail%3FsellerId%3D4151565603%26activityId%3D388a6031781e4ee4981164685ab86839&amp;commissionRate=30&amp;voucher_pass=&amp;life=0"> 
-      <img src="https://img.alicdn.com/imgextra/i4/2863069160/O1CN018KR3Nx2HXKUI4LwSK_!!2863069160.jpg_320x320.jpg">
+    <a href="/download-app"> 
+      <img src="/clientapp/images/newbie/1.jpg">
     </a>
   </div>
   <div class="txtBox flex1">
-    <h2 class="name">垃圾袋家用手提式加厚黑色宿舍用学生大号拉圾塑料袋抽绳自动收口</h2>
+    <h2 class="name">海底捞醇香麻辣牛油火锅料150g</h2>
     <div class="typeBox">
-      <span class="type-red">1元券</span>
-      <span class="type-price">淘宝<em>¥</em>6.9 | 销量118.88万</span>
-      <!-- <span class="type-sred">返177积分</span> -->
-    
+      <span class="type-red">3元券</span>
+      <span class="type-price">淘宝<em>¥</em>19.9 | 销量3.4万</span>
     </div>
     <div class="moneyBox">
       <div class="amount"><em>¥</em>0</div>
@@ -18,19 +17,18 @@
     </div>
   </div>
 </div>
+
 <div class="inBox">
   <div class="imgBox">
-    <a href="/main/product/detail?id=260&amp;goodsId=590084304040&amp;mainPic=https://img.alicdn.com/imgextra/i4/2863069160/O1CN018KR3Nx2HXKUI4LwSK_!!2863069160.jpg&amp;title=垃圾袋家用手提式加厚黑色宿舍用学生大号拉圾塑料袋抽绳自动收口&amp;monthSales=1188752&amp;originalPrice=6.9&amp;couponPrice=1&amp;couponLink=https%3A%2F%2Fuland.taobao.com%2Fquan%2Fdetail%3FsellerId%3D4151565603%26activityId%3D388a6031781e4ee4981164685ab86839&amp;commissionRate=30&amp;voucher_pass=&amp;life=0"> 
-      <img src="https://img.alicdn.com/imgextra/i4/2863069160/O1CN018KR3Nx2HXKUI4LwSK_!!2863069160.jpg_320x320.jpg">
+    <a href="/download-app"> 
+      <img src="/clientapp/images/newbie/2.jpg">
     </a>
   </div>
   <div class="txtBox flex1">
-    <h2 class="name">垃圾袋家用手提式加厚黑色宿舍用学生大号拉圾塑料袋抽绳自动收口</h2>
+    <h2 class="name">谷韵乳酸菌酸牛奶100ml*20瓶</h2>
     <div class="typeBox">
-      <span class="type-red">1元券</span>
-      <span class="type-price">淘宝<em>¥</em>6.9 | 销量118.88万</span>
-      <!-- <span class="type-sred">返177积分</span> -->
-    
+      <span class="type-red">4元券</span>
+      <span class="type-price">淘宝<em>¥</em>19.9 | 销量3万</span>
     </div>
     <div class="moneyBox">
       <div class="amount"><em>¥</em>0</div>
@@ -38,19 +36,18 @@
     </div>
   </div>
 </div>
+
 <div class="inBox">
   <div class="imgBox">
-    <a href="/main/product/detail?id=260&amp;goodsId=590084304040&amp;mainPic=https://img.alicdn.com/imgextra/i4/2863069160/O1CN018KR3Nx2HXKUI4LwSK_!!2863069160.jpg&amp;title=垃圾袋家用手提式加厚黑色宿舍用学生大号拉圾塑料袋抽绳自动收口&amp;monthSales=1188752&amp;originalPrice=6.9&amp;couponPrice=1&amp;couponLink=https%3A%2F%2Fuland.taobao.com%2Fquan%2Fdetail%3FsellerId%3D4151565603%26activityId%3D388a6031781e4ee4981164685ab86839&amp;commissionRate=30&amp;voucher_pass=&amp;life=0"> 
-      <img src="https://img.alicdn.com/imgextra/i4/2863069160/O1CN018KR3Nx2HXKUI4LwSK_!!2863069160.jpg_320x320.jpg">
+    <a href="/download-app"> 
+      <img src="/clientapp/images/newbie/3.png">
     </a>
   </div>
   <div class="txtBox flex1">
-    <h2 class="name">垃圾袋家用手提式加厚黑色宿舍用学生大号拉圾塑料袋抽绳自动收口</h2>
+    <h2 class="name">自动软毛美白震动电动牙刷智能净白</h2>
     <div class="typeBox">
-      <span class="type-red">1元券</span>
-      <span class="type-price">淘宝<em>¥</em>6.9 | 销量118.88万</span>
-      <!-- <span class="type-sred">返177积分</span> -->
-    
+      <span class="type-red">20元券</span>
+      <span class="type-price">淘宝<em>¥</em>39.9 | 销量2.3万</span>
     </div>
     <div class="moneyBox">
       <div class="amount"><em>¥</em>0</div>
@@ -58,19 +55,18 @@
     </div>
   </div>
 </div>
+
 <div class="inBox">
   <div class="imgBox">
-    <a href="/main/product/detail?id=260&amp;goodsId=590084304040&amp;mainPic=https://img.alicdn.com/imgextra/i4/2863069160/O1CN018KR3Nx2HXKUI4LwSK_!!2863069160.jpg&amp;title=垃圾袋家用手提式加厚黑色宿舍用学生大号拉圾塑料袋抽绳自动收口&amp;monthSales=1188752&amp;originalPrice=6.9&amp;couponPrice=1&amp;couponLink=https%3A%2F%2Fuland.taobao.com%2Fquan%2Fdetail%3FsellerId%3D4151565603%26activityId%3D388a6031781e4ee4981164685ab86839&amp;commissionRate=30&amp;voucher_pass=&amp;life=0"> 
-      <img src="https://img.alicdn.com/imgextra/i4/2863069160/O1CN018KR3Nx2HXKUI4LwSK_!!2863069160.jpg_320x320.jpg">
+    <a href="/download-app"> 
+      <img src="/clientapp/images/newbie/4.jpg">
     </a>
   </div>
   <div class="txtBox flex1">
-    <h2 class="name">垃圾袋家用手提式加厚黑色宿舍用学生大号拉圾塑料袋抽绳自动收口</h2>
+    <h2 class="name">洁柔抽纸Face纸巾3层20包可湿水小便携装家用卫生面纸巾批发包邮</h2>
     <div class="typeBox">
-      <span class="type-red">1元券</span>
-      <span class="type-price">淘宝<em>¥</em>6.9 | 销量118.88万</span>
-      <!-- <span class="type-sred">返177积分</span> -->
-    
+      <span class="type-red">5元券</span>
+      <span class="type-price">淘宝<em>¥</em>39.9 | 销量12.2万</span>
     </div>
     <div class="moneyBox">
       <div class="amount"><em>¥</em>0</div>
@@ -78,19 +74,18 @@
     </div>
   </div>
 </div>
+
 <div class="inBox">
   <div class="imgBox">
-    <a href="/main/product/detail?id=260&amp;goodsId=590084304040&amp;mainPic=https://img.alicdn.com/imgextra/i4/2863069160/O1CN018KR3Nx2HXKUI4LwSK_!!2863069160.jpg&amp;title=垃圾袋家用手提式加厚黑色宿舍用学生大号拉圾塑料袋抽绳自动收口&amp;monthSales=1188752&amp;originalPrice=6.9&amp;couponPrice=1&amp;couponLink=https%3A%2F%2Fuland.taobao.com%2Fquan%2Fdetail%3FsellerId%3D4151565603%26activityId%3D388a6031781e4ee4981164685ab86839&amp;commissionRate=30&amp;voucher_pass=&amp;life=0"> 
-      <img src="https://img.alicdn.com/imgextra/i4/2863069160/O1CN018KR3Nx2HXKUI4LwSK_!!2863069160.jpg_320x320.jpg">
+    <a href="/download-app"> 
+      <img src="/clientapp/images/newbie/5.jpg">
     </a>
   </div>
   <div class="txtBox flex1">
-    <h2 class="name">垃圾袋家用手提式加厚黑色宿舍用学生大号拉圾塑料袋抽绳自动收口</h2>
+    <h2 class="name">白象大辣椒韩式火鸡面112gX5袋装整箱干拌面国产泡面方便面宿舍</h2>
     <div class="typeBox">
-      <span class="type-red">1元券</span>
-      <span class="type-price">淘宝<em>¥</em>6.9 | 销量118.88万</span>
-      <!-- <span class="type-sred">返177积分</span> -->
-    
+      <span class="type-red">2元券</span>
+      <span class="type-price">淘宝<em>¥</em>19.9 | 销量1.2万</span>
     </div>
     <div class="moneyBox">
       <div class="amount"><em>¥</em>0</div>
@@ -98,19 +93,18 @@
     </div>
   </div>
 </div>
+
 <div class="inBox">
   <div class="imgBox">
-    <a href="/main/product/detail?id=260&amp;goodsId=590084304040&amp;mainPic=https://img.alicdn.com/imgextra/i4/2863069160/O1CN018KR3Nx2HXKUI4LwSK_!!2863069160.jpg&amp;title=垃圾袋家用手提式加厚黑色宿舍用学生大号拉圾塑料袋抽绳自动收口&amp;monthSales=1188752&amp;originalPrice=6.9&amp;couponPrice=1&amp;couponLink=https%3A%2F%2Fuland.taobao.com%2Fquan%2Fdetail%3FsellerId%3D4151565603%26activityId%3D388a6031781e4ee4981164685ab86839&amp;commissionRate=30&amp;voucher_pass=&amp;life=0"> 
-      <img src="https://img.alicdn.com/imgextra/i4/2863069160/O1CN018KR3Nx2HXKUI4LwSK_!!2863069160.jpg_320x320.jpg">
+    <a href="/download-app"> 
+      <img src="/clientapp/images/newbie/6.jpg">
     </a>
   </div>
   <div class="txtBox flex1">
-    <h2 class="name">垃圾袋家用手提式加厚黑色宿舍用学生大号拉圾塑料袋抽绳自动收口</h2>
+    <h2 class="name">红酒整箱干红葡萄酒澳大利亚进口西拉色拉子干红澳洲红酒礼袋送礼</h2>
     <div class="typeBox">
-      <span class="type-red">1元券</span>
-      <span class="type-price">淘宝<em>¥</em>6.9 | 销量118.88万</span>
-      <!-- <span class="type-sred">返177积分</span> -->
-    
+      <span class="type-red">15元券</span>
+      <span class="type-price">淘宝<em>¥</em>59.9 | 销量8731件</span>
     </div>
     <div class="moneyBox">
       <div class="amount"><em>¥</em>0</div>
@@ -118,19 +112,18 @@
     </div>
   </div>
 </div>
+
 <div class="inBox">
   <div class="imgBox">
-    <a href="/main/product/detail?id=260&amp;goodsId=590084304040&amp;mainPic=https://img.alicdn.com/imgextra/i4/2863069160/O1CN018KR3Nx2HXKUI4LwSK_!!2863069160.jpg&amp;title=垃圾袋家用手提式加厚黑色宿舍用学生大号拉圾塑料袋抽绳自动收口&amp;monthSales=1188752&amp;originalPrice=6.9&amp;couponPrice=1&amp;couponLink=https%3A%2F%2Fuland.taobao.com%2Fquan%2Fdetail%3FsellerId%3D4151565603%26activityId%3D388a6031781e4ee4981164685ab86839&amp;commissionRate=30&amp;voucher_pass=&amp;life=0"> 
-      <img src="https://img.alicdn.com/imgextra/i4/2863069160/O1CN018KR3Nx2HXKUI4LwSK_!!2863069160.jpg_320x320.jpg">
+    <a href="/download-app"> 
+      <img src="/clientapp/images/newbie/7.jpg">
     </a>
   </div>
   <div class="txtBox flex1">
-    <h2 class="name">垃圾袋家用手提式加厚黑色宿舍用学生大号拉圾塑料袋抽绳自动收口</h2>
+    <h2 class="name">水果燕麦片即食非无糖脱脂营养酸奶果粒冲饮坚果早餐速食懒人食品</h2>
     <div class="typeBox">
-      <span class="type-red">1元券</span>
-      <span class="type-price">淘宝<em>¥</em>6.9 | 销量118.88万</span>
-      <!-- <span class="type-sred">返177积分</span> -->
-    
+      <span class="type-red">20元券</span>
+      <span class="type-price">淘宝<em>¥</em>34.8 | 销量22.2万</span>
     </div>
     <div class="moneyBox">
       <div class="amount"><em>¥</em>0</div>
@@ -138,39 +131,18 @@
     </div>
   </div>
 </div>
+
 <div class="inBox">
   <div class="imgBox">
-    <a href="/main/product/detail?id=260&amp;goodsId=590084304040&amp;mainPic=https://img.alicdn.com/imgextra/i4/2863069160/O1CN018KR3Nx2HXKUI4LwSK_!!2863069160.jpg&amp;title=垃圾袋家用手提式加厚黑色宿舍用学生大号拉圾塑料袋抽绳自动收口&amp;monthSales=1188752&amp;originalPrice=6.9&amp;couponPrice=1&amp;couponLink=https%3A%2F%2Fuland.taobao.com%2Fquan%2Fdetail%3FsellerId%3D4151565603%26activityId%3D388a6031781e4ee4981164685ab86839&amp;commissionRate=30&amp;voucher_pass=&amp;life=0"> 
-      <img src="https://img.alicdn.com/imgextra/i4/2863069160/O1CN018KR3Nx2HXKUI4LwSK_!!2863069160.jpg_320x320.jpg">
+    <a href="/download-app"> 
+      <img src="/clientapp/images/newbie/8.jpg">
     </a>
   </div>
   <div class="txtBox flex1">
-    <h2 class="name">垃圾袋家用手提式加厚黑色宿舍用学生大号拉圾塑料袋抽绳自动收口</h2>
+    <h2 class="name">7双袜子女中筒袜日系素色棉袜秋冬黑白色夏季厚款百搭纯色短袜长筒袜</h2>
     <div class="typeBox">
-      <span class="type-red">1元券</span>
-      <span class="type-price">淘宝<em>¥</em>6.9 | 销量118.88万</span>
-      <!-- <span class="type-sred">返177积分</span> -->
-    
-    </div>
-    <div class="moneyBox">
-      <div class="amount"><em>¥</em>0</div>
-      <div class="txt">APP专享补贴</div>
-    </div>
-  </div>
-</div>
-<div class="inBox">
-  <div class="imgBox">
-    <a href="/main/product/detail?id=260&amp;goodsId=590084304040&amp;mainPic=https://img.alicdn.com/imgextra/i4/2863069160/O1CN018KR3Nx2HXKUI4LwSK_!!2863069160.jpg&amp;title=垃圾袋家用手提式加厚黑色宿舍用学生大号拉圾塑料袋抽绳自动收口&amp;monthSales=1188752&amp;originalPrice=6.9&amp;couponPrice=1&amp;couponLink=https%3A%2F%2Fuland.taobao.com%2Fquan%2Fdetail%3FsellerId%3D4151565603%26activityId%3D388a6031781e4ee4981164685ab86839&amp;commissionRate=30&amp;voucher_pass=&amp;life=0"> 
-      <img src="https://img.alicdn.com/imgextra/i4/2863069160/O1CN018KR3Nx2HXKUI4LwSK_!!2863069160.jpg_320x320.jpg">
-    </a>
-  </div>
-  <div class="txtBox flex1">
-    <h2 class="name">垃圾袋家用手提式加厚黑色宿舍用学生大号拉圾塑料袋抽绳自动收口</h2>
-    <div class="typeBox">
-      <span class="type-red">1元券</span>
-      <span class="type-price">淘宝<em>¥</em>6.9 | 销量118.88万</span>
-      <!-- <span class="type-sred">返177积分</span> -->
-    
+      <span class="type-red">8元券</span>
+      <span class="type-price">淘宝<em>¥</em>24.98 | 销量1.2万</span>
     </div>
     <div class="moneyBox">
       <div class="amount"><em>¥</em>0</div>
