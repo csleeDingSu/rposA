9dcbbe9be9921a7a97e7235e90d4d9cfa2446a43
csleeDingsu <cslee.dingsu@gmail.com>
2019-11-13 08:17:22 +0800

    Update tabaoApiController.php

--- app/Http/Controllers/tabaoApiController.php
+++ app/Http/Controllers/tabaoApiController.php
@@ -110,7 +110,7 @@ class tabaoApiController extends BaseController
 
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-        curl_setopt($ch,CURLOPT_TIMEOUT,50);
+        curl_setopt($ch,CURLOPT_TIMEOUT,60);
 
         curl_setopt($ch, CURLOPT_HEADER, 0);
 
