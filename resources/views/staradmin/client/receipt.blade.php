0ad6d0364f0112f14c022a58db13a6e2c2ef21b5
csleeDingsu <cslee.dingsu@gmail.com>
2019-11-13 17:35:13 +0800

    Update receipt.blade.php

--- resources/views/staradmin/client/receipt.blade.php
+++ resources/views/staradmin/client/receipt.blade.php
@@ -111,3 +111,26 @@
     </script>
 
 @endsection
+
+<!-- socket start-->  
+<script type="text/javascript">
+  @section('socket')
+  @parent
+
+    var id = "{{isset(Auth::Guard('member')->user()->id) ? Auth::Guard('member')->user()->id : 0}}";
+    console.log('prefix --- ' + prefix);
+    console.log('id --- ' + id);
+
+    socket.on(prefix+"new-receipt" + ":App\\Events\\EventDynamicChannel" , function(data){
+        console.log(prefix+"new-receipt" + ":App\\Events\\EventDynamicChannel");
+        console.log(data.data);
+    });
+
+    socket.on(prefix+"receipt-updated" + ":App\\Events\\EventDynamicChannel" , function(data){
+        console.log(prefix+"receipt-updated" + ":App\\Events\\EventDynamicChannel");
+        console.log(data.data);
+    });
+    
+  @endsection
+</script>
+<!-- socket end-->
