48a2bc5390175c585f479926a6826c2e83a1cff3
prem adarsh <premadarsh.it@gmail.com>
2019-11-13 14:09:19 +0800

    Update CreditController.php

--- app/Http/Controllers/Api/CreditController.php
+++ app/Http/Controllers/Api/CreditController.php
@@ -86,7 +86,7 @@ class CreditController extends Controller
                         })
                         ->get();
                     print_r( $pending);
-        if ($pending)
+        if ($pending->isNotEmpty)
         {
             return response()->json(['success' => false, 'errormessage'=>'pending_record']);
         }
