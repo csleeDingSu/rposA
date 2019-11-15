b191313a3733fd8363de4a64f4eaf57f018ed618
prem adarsh <premadarsh.it@gmail.com>
2019-11-15 09:00:03 +0800

    phone removed

--- resources/views/staradmin/resell/account/render_data.blade.php
+++ resources/views/staradmin/resell/account/render_data.blade.php
@@ -1,12 +1,11 @@
 @foreach( $result as $list )
 <tr id="tr_{{$list->id}}" class="{{$highlight ?? ''}}" >
 	<td scope="row">{{$list->id}}</td>
-	<td>{{$list->name}}</td>
+	
 	<td>{{$list->bank_detail}}</td>
 	<td>{{$list->account_name}}</td>
 	<td>{{$list->account_number ?? '-' }}</td>
-	<td>{{$list->bank_name ?? '-' }}</td>
-	<td>{{$list->phone ?? '-'  }}</td>	
+	
 	<td>
 		<button type="button" data-id="{{$list->id}}" id="{{$list->id}}" class="btn btn-icons btn-rounded btn-outline-info btn-inverse-success editrow"> <i class="fa fa-pencil-alt"></i> </button>	
 		<button type="button" data-id="{{$list->id}}" id="{{$list->id}} rejectrow" class="btn btn-icons btn-rounded btn-outline-danger btn-inverse-danger rejectrow"> <i class="fa fa-trash"></i> </button>
