05cca34826f56662bb471ebddec816b307f54aef
prem adarsh <premadarsh.it@gmail.com>
2019-11-19 16:36:16 +0800

    Update model.blade.php

--- resources/views/staradmin/member/model.blade.php
+++ resources/views/staradmin/member/model.blade.php
@@ -25,3 +25,51 @@
 	</div>
 </form>
 <!-- Modal Ends -->
+
+
+
+
+
+
+<!-- Ali pay starts -->
+<form class="form-sample" name="formalipay" id="formedit" action="" method="post" autocomplete="on" enctype="multipart/form-data">
+	<div class="modal fade" id="openalipaymodel" tabindex="-1" role="dialog" aria-labelledby="openalipaymodellabel" aria-hidden="true">
+		<div class="modal-dialog modal-lg" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title formtitle" id="editmodelabel formtitle">@lang('dingsu.alipay') </h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					 	<span aria-hidden="true">&times;</span>
+				  	</button>
+					@csrf
+				</div>
+				<div class="modal-body renderdata">										
+					<div class="row">
+						<div class="col-md-6">
+							<div class="form-group row">
+								<label for="amount" class="col-sm-3 col-form-label">@lang('dingsu.payee_account') <span class="text-danger">*</span></label>
+								<div class="col-sm-9">
+									<input id="payee_account" name="payee_account" class="form-control" type="text" value="" maxlength="50">
+								</div>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="form-group row">
+								<label for="amount" class="col-sm-3 col-form-label">@lang('dingsu.amount') <span class="text-danger">*</span></label>
+								<div class="col-sm-9">
+									<input id="amount" name="amount" class="form-control" type="text" value="" maxlength="50">
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button type="submit" class="btn btn-success" id="savebtn">@lang('dingsu.update')</button>
+					<button type="button" class="btn btn-dark" data-dismiss="modal">@lang('dingsu.cancel')</button>
+				</div>
+				<input type="hidden" name="mode" id="mode" value="edit">
+			</div>
+		</div>
+	</div>
+</form>
+<!-- Modal Ends -->
